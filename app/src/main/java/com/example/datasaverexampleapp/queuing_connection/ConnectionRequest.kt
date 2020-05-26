acf822f429d8a379ba22e1d364ce10d4f44d8c37
mel182 <vrol0004@gmail.com>
2020-05-26 06:39:19 +0200

    connection queuing examples added

---
+++ app/src/main/java/com/example/datasaverexampleapp/queuing_connection/ConnectionRequest.kt
@@ -0,0 +1,3 @@
+package com.example.datasaverexampleapp.queuing_connection
+
+data class ConnectionRequest(val url:String)
---
+++ app/src/main/java/com/example/datasaverexampleapp/queuing_connection/QueuingConnection.kt
@@ -0,0 +1,21 @@
+package com.example.datasaverexampleapp.queuing_connection
+
+import java.util.*
+
+class QueuingConnection {
+
+    private val requests: Queue<ConnectionRequest> = LinkedList()
+
+    fun addRequest(request:ConnectionRequest)
+    {
+        requests.add(request)
+
+        val headQueue = requests.peek() //Get head queue of the list
+        requests.remove() // Remove the head queue from the list
+    }
+
+    fun execute()
+    {
+        // Iteration over the queue for execution
+    }
+}
