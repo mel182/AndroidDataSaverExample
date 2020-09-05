c47b57ff7a8c6df623bb77d2d0fd58dc926d0461
Melchior Vrolijk <vrol0004@gmail.com>
2020-09-06 00:11:49 +0200

    more anima example added

--- app/src/main/java/com/example/datasaverexampleapp/animation/AnimationExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/animation/AnimationExampleActivity.kt
@@ -1,8 +1,12 @@
 package com.example.datasaverexampleapp.animation
 
 import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
 import com.example.datasaverexampleapp.R
 import kotlinx.android.synthetic.main.activity_animation_example.*
 
@@ -15,6 +19,20 @@ class AnimationExampleActivity : AppCompatActivity() {
             val alphaAnimator = AnimatorInflater.loadAnimator(this@AnimationExampleActivity, R.animator.set_alpha_between_0_1)
             alphaAnimator.setTarget(this)
             alphaAnimator.start()
+        }
+
+        group_animation_example?.apply {
+
+            setOnClickListener {
+                val rotationShrinkAnimation = AnimationUtils.loadAnimation(this@AnimationExampleActivity, R.anim.rotation_shrink)
+                startAnimation(rotationShrinkAnimation)
+            }
+        }
+
+        imageViewAnimation?.apply {
+            setBackgroundResource(R.drawable.image_animation)
+            val animationDrawable = background as AnimationDrawable
+            animationDrawable.start()
         }
     }
 }
