e4a7d6dd5ac57600b6fb9157c2c1fa85ad12cd69
Melchior Vrolijk <vrol0004@gmail.com>
2020-09-06 11:11:32 +0200

    more example added

--- app/src/main/java/com/example/datasaverexampleapp/MainActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/MainActivity.kt
@@ -22,6 +22,7 @@ import com.example.datasaverexampleapp.inDefInterfaces.Constants
 import com.example.datasaverexampleapp.inDefInterfaces.Shape
 import com.example.datasaverexampleapp.resourceTest.ResourceActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 import java.math.BigInteger
 
 
@@ -210,35 +211,38 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkActiveNetwork()
     {
-        // Check if the active network is metered and determine the restrictions on this app by the user.
-        val connectionManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val status = connectionManager.restrictBackgroundStatus
-
-        Log.i("TAG","Get restricted background status: $status")
-
-        //Check if the active network is metered one
-        if (connectionManager.isActiveNetworkMetered)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
         {
-            Log.i("TAG","Active network is metered")
-            // Checks user's Data saver preference
-            when(connectionManager.restrictBackgroundStatus)
+            // Check if the active network is metered and determine the restrictions on this app by the user.
+            val connectionManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+            val status = connectionManager.restrictBackgroundStatus
+
+            Log.i("TAG","Get restricted background status: $status")
+
+            //Check if the active network is metered one
+            if (connectionManager.isActiveNetworkMetered)
             {
-                RESTRICT_BACKGROUND_STATUS_ENABLED -> {
-                    // Data saver is enabled and the application shouldn't use the network in background
-                    Log.i("TAG","Restricted background status enabled")}
-                RESTRICT_BACKGROUND_STATUS_WHITELISTED -> {
-                    //Data saver is enabled, but the application is whitelisted. The application should limit the
-                    // network request while the Data Saver is enabled even if the application is whitelisted
-                    Log.i("TAG","Restricted background status whitelisted")}
-                RESTRICT_BACKGROUND_STATUS_DISABLED -> {
-                    // Data Saver is disabled
-                    Log.i("TAG","Restricted background status disabled")
+                Log.i("TAG","Active network is metered")
+                // Checks user's Data saver preference
+                when(connectionManager.restrictBackgroundStatus)
+                {
+                    RESTRICT_BACKGROUND_STATUS_ENABLED -> {
+                        // Data saver is enabled and the application shouldn't use the network in background
+                        Log.i("TAG","Restricted background status enabled")}
+                    RESTRICT_BACKGROUND_STATUS_WHITELISTED -> {
+                        //Data saver is enabled, but the application is whitelisted. The application should limit the
+                        // network request while the Data Saver is enabled even if the application is whitelisted
+                        Log.i("TAG","Restricted background status whitelisted")}
+                    RESTRICT_BACKGROUND_STATUS_DISABLED -> {
+                        // Data Saver is disabled
+                        Log.i("TAG","Restricted background status disabled")
+                    }
                 }
+            } else {
+                // Active network is not metered so any network request can be done
+                Log.i("TAG","The active network is not metered")
             }
-        } else {
-            // Active network is not metered so any network request can be done
-            Log.i("TAG","The active network is not metered")
         }
     }
 
