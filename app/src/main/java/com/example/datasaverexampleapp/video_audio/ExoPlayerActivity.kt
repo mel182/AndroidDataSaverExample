734a2141e219942427383d21a375b0a50eac94e8
Melchior Vrolijk <vrol0004@gmail.com>
2021-03-21 22:58:13 +0100

    audio playback example in progress

--- app/src/main/java/com/example/datasaverexampleapp/video_audio/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/ExoPlayerActivity.kt
@@ -1,14 +1,95 @@
 package com.example.datasaverexampleapp.video_audio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.example.datasaverexampleapp.R
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+import com.google.android.exoplayer2.util.Util
+import kotlinx.android.synthetic.main.activity_exo_player.*
+import java.io.File
 
+
+/**
+ * ExoPlayer library has been build by Google to provide consistent experience,
+ * better extensibility, and additional format support for media playback on all
+ * devices running Android 4.1 (API Level 16) or higher
+ *
+ * Dependencies:
+ * The 'exoplayer-core' library is the only required dependency for integrating ExoPlayer
+ * into your app; however, ExoPlayer also provide a number of subcomponents that offer
+ * additional functionality. For example, the 'exoplayer-ui' library provides pre-built
+ * UI components that greatly simplify common operations including playback controls.
+ *
+ * Usage:
+ * To use ExoPlayer for video playback, you must add the ExoPlayer core and UI libraries as
+ * dependencies to your module's 'build.gradle' file:
+ *
+ * implementation 'com.google.android.exoplayer:exoplayer-core:2.8.2'
+ * implementation 'com.google.android.exoplayer:exoplayer-ui:2.8.2'
+ *
+ * The ExoPlayer UI library provides a 'PlayerView' class that encapsulates
+ * both the playback surface and playback controls including play,pause,
+ * fast forward, rewind and a seek bar for skipping through the video, and
+ * which can be added to your Activity or Fragment layout.
+ */
 class ExoPlayerActivity : AppCompatActivity()
 {
+    private var player:SimpleExoPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exo_player)
         title = "Exoplayer Example (api 16+)"
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Create a new Exo Player
+        player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+
+        // associate the ExoPlayer with the player View
+        player_view?.player = player
+
+        //Build a DataSource.Factory capable of loading http and local content
+        val dataSourceFactory = DefaultDataSourceFactory(
+            this, Util.getUserAgent(
+                this,
+                getString(R.string.app_name)
+            )
+        )
+
+        // This is the MediaSource representing the media to be played.
+        val rawDirectoryResource: MediaSource = ExtractorMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(RawResourceDataSource.buildRawResourceUri(R.raw.sample_video))
+
+        // Start loading the media source
+        player?.prepare(rawDirectoryResource)
+
+        // Start playback automatically when ready
+        player?.playWhenReady = true
+    }
+
+
+    override fun onStop() {
+        player_view?.player = null
+        player?.release()
+        player = null
+        super.onStop()
     }
 }
--- app/src/main/java/com/example/datasaverexampleapp/video_audio/VideoAudioStreamingActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/VideoAudioStreamingActivity.kt
@@ -6,6 +6,55 @@ import android.os.Bundle
 import com.example.datasaverexampleapp.R
 import kotlinx.android.synthetic.main.activity_video_audio_streaming.*
 
+/**
+ * Smartphones and tablets have become so popular that for many
+ * people they have entirely replaced all other portable electronics -
+ * including, cameras, music players, and audio recorders. As a result
+ * Android's media APIs, which allow us to build apps that offer a rich
+ * audio, video and camera experience, have become increasingly powerful
+ * and important.
+ *
+ * Android 8.1 Oreo (API Level 27) supports the following multimedia formats
+ * for playback as part of the framework. Note that some devices may support
+ * playback of additional file formats:
+ * AUDIO
+ * - ACC LC
+ * - HE-ACCv1 (AAC+)
+ * - HE-ACCv2 (Enhanced AAC+)
+ * - ACC ELD (Enhanced Low Delay AAC)
+ * - AMR-NB
+ * - AMR-WB
+ * - FLAC
+ * - MP3
+ * - MIDI
+ * - Ogg Vorbis
+ * - PCM/WAVE
+ * - Opus
+ *
+ * IMAGE
+ * - JPEG
+ * - PNG
+ * - WEBP
+ * - GIF
+ * - BMP
+ *
+ * VIDEO
+ * - H.263
+ * - H.264 AVC
+ * - H.265 HEVC
+ * - MPEG-4 SP
+ * - VP8
+ * - VP9
+ *
+ * The following network protocols are supported for streaming media:
+ * - RTSP (RTP, SDP)
+ * - HTTP/HTTPS progressive streaming
+ * - HTTP/HTTPS live streaming (on device running Android 3.0 or above)
+ *
+ * Note: For full details on the currently supported media formats and recommendations for video
+ *       encoding and audio streaming, see the Supported Media Formats page on the Android Developer
+ *       documentation site.
+ */
 class VideoAudioStreamingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +68,11 @@ class VideoAudioStreamingActivity : AppCompatActivity() {
 
         exo_player_example?.setOnClickListener {
             val intent = Intent(this, ExoPlayerActivity::class.java)
+            startActivity(intent)
+        }
+
+        audio_playback_example?.setOnClickListener {
+            val intent = Intent(this, AudioPlaybackExampleActivity::class.java)
             startActivity(intent)
         }
 
