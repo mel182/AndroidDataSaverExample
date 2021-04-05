0a2f7e1b432808a30318eee587f71458ab0dad9b
Melchior Vrolijk <vrol0004@gmail.com>
2021-04-05 23:07:11 +0200

    media player fragment code implemented

--- app/src/main/java/com/example/datasaverexampleapp/video_audio/background_audio/MediaPlayerFragment.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/background_audio/MediaPlayerFragment.kt
@@ -1,10 +1,19 @@
 package com.example.datasaverexampleapp.video_audio.background_audio
 
+import android.content.ComponentName
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.RemoteException
+import android.os.ResultReceiver
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.session.MediaControllerCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.datasaverexampleapp.R
 import com.example.datasaverexampleapp.type_alias.Layout
 import kotlinx.android.synthetic.main.item_player_layout.*
@@ -16,11 +25,8 @@ import kotlinx.android.synthetic.main.item_player_layout.*
  */
 class MediaPlayerFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var mediaBrowser: MediaBrowserCompat? = null
+    private var mediaController: MediaControllerCompat? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -31,5 +37,184 @@ class MediaPlayerFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         playerTitle?.text = "Media player"
+        updateStatus("Initializing....")
+
+        activity?.let { fragmentActivity ->
+
+            mediaBrowser = MediaBrowserCompat(fragmentActivity,
+                ComponentName(fragmentActivity, ExoPlayerMediaPlaybackService::class.java),
+                object : MediaBrowserCompat.ConnectionCallback(){
+
+                    override fun onConnected() {
+                        super.onConnected()
+                        initializeMediaBrowserAndController()
+                        updateStatus("-")
+                    }
+
+                    override fun onConnectionSuspended() {
+                        super.onConnectionSuspended()
+                        // We were connected, but no longer are.
+                        updateStatus("Service connection suspended")
+                    }
+
+                    override fun onConnectionFailed() {
+                        super.onConnectionFailed()
+                        // The attempt to connect failed completely.
+                        // Check the ComponentName!
+                        updateStatus("Service Connection failed")
+                    }
+                },null)
+
+            mediaBrowser?.connect()
+        }
+
+        playAudioButton?.setOnClickListener {
+
+            when (playAudioButton?.text)
+            {
+                "Add Media Audio" -> {
+                    updateStatus("Added media....")
+                    addPlayAudio(R.raw.bon_vibe)
+                    streamAudioButton?.isEnabled = false
+                }
+                "Play Audio" -> {
+                    mediaController?.transportControls?.play()
+                    playAudioButton?.text = "Stop Audio"
+                    updateStatus("Preparing....")
+                }
+                "Stop Audio" -> {
+                    mediaController?.transportControls?.stop()
+                    updateStatus("-")
+                    playAudioButton?.text = "Add Media Audio"
+                    streamAudioButton?.isEnabled = true
+                }
+            }
+        }
+
+        streamAudioButton?.setOnClickListener {
+
+            when (streamAudioButton?.text)
+            {
+                "Add Stream Audio" -> {
+                    updateStatus("Added streaming media....")
+                    addPlayAudio("https://stream.audioxi.com/SW")
+                    playAudioButton?.isEnabled = false
+                }
+                "Play Stream Audio" -> {
+                    mediaController?.transportControls?.play()
+                    streamAudioButton?.text = "Stop Streaming Audio"
+                }
+                "Stop Streaming Audio" -> {
+                    mediaController?.transportControls?.stop()
+                    updateStatus("-")
+                    streamAudioButton?.text = "Add Stream Audio"
+                    playAudioButton?.isEnabled = true
+                }
+            }
+        }
     }
+
+    private fun updateStatus(status:String)
+    {
+        streamStatus?.text = status
+    }
+
+    private fun initializeMediaBrowserAndController()
+    {
+        try {
+            // We can construct a media controller from the session's token
+            mediaBrowser?.let {
+
+                mediaController = MediaControllerCompat(activity, it.sessionToken).apply {
+
+                    // To ensure your UI stays in sync with your Service, register a 'MediaControllerCompat.Callback' using the 'registerCallback'
+                    // method on the Media Controller. This will ensure you receive a callback whenever the metadata or playback state changes, allowing
+                    // you to keep your UI updated at all times.
+                    registerCallback(object : MediaControllerCompat.Callback(){
+
+                        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+                            // Update the UI based on playback state change
+
+                            state?.let{
+
+                                if (it.actions == PlaybackStateCompat.ACTION_PLAY_PAUSE && it.state == PlaybackStateCompat.STATE_PLAYING)
+                                {
+                                    updateStatus("Playing...")
+                                    getParentActivity()?.getPlayerTabLayout()?.visibility = View.GONE
+
+                                } else if (it.actions == PlaybackStateCompat.ACTION_PLAY_FROM_URI && it.state == PlaybackStateCompat.STATE_PLAYING)
+                                {
+                                    updateStatus("Streaming...")
+                                    getParentActivity()?.getPlayerTabLayout()?.visibility = View.GONE
+                                } else if (it.actions == PlaybackStateCompat.ACTION_STOP && it.state == PlaybackStateCompat.STATE_STOPPED)
+                                {
+                                    Log.i("TAG","Action stop!")
+                                    updateStatus("-")
+                                    playAudioButton?.isEnabled = true
+                                    streamAudioButton?.isEnabled = true
+                                    getParentActivity()?.getPlayerTabLayout()?.visibility = View.VISIBLE
+                                }
+                            }
+                        }
+
+                        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+                            // Update the UI based on Media Metadata change
+                        }
+                    })
+                }
+            }
+        }catch (e: RemoteException)
+        {
+            Log.i("TAG","Error creating controller: ${e.message}")
+        }
+    }
+
+    private fun addPlayAudio(mediaSource:Any)
+    {
+        val mediaBundle : Bundle = Bundle().apply {
+
+            if (mediaSource is Int)
+                putInt(MEDIA_SOURCE,mediaSource)
+            else if (mediaSource is String)
+                putString(MEDIA_SOURCE,mediaSource)
+        }
+
+        mediaSource?.let {
+            mediaController?.sendCommand(ADD_MEDIA_SOURCE, mediaBundle, object : ResultReceiver(
+                Handler()
+            )
+            {
+                override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+                    super.onReceiveResult(resultCode, resultData)
+                    streamStatus?.text = if (resultCode == MEDIA_SOURCE_ADDED) "Media added, ready to play" else "Failed to add media, try again"
+
+                    if (mediaSource is Int)
+                    {
+                        playAudioButton?.text = "Play Audio"
+                    } else if (mediaSource is String)
+                    {
+                        streamAudioButton?.text = "Play Stream Audio"
+                    }
+                }
+            })
+        }?: kotlin.run {
+            updateStatus("Failed adding media source, try again")
+            playAudioButton?.isEnabled = true
+            streamAudioButton?.isEnabled = true
+        }
+    }
+
+    private fun getParentActivity() : BackgroundAudioExampleActivity?
+    {
+        return if (activity is BackgroundAudioExampleActivity)
+            activity as BackgroundAudioExampleActivity
+        else
+            null
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaBrowser?.disconnect()
+    }
+
 }
--- app/src/main/java/com/example/datasaverexampleapp/video_audio/background_audio/MediaPlayerPlaybackService.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/background_audio/MediaPlayerPlaybackService.kt
@@ -1,22 +1,248 @@
 package com.example.datasaverexampleapp.video_audio.background_audio
 
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
 import android.media.AudioManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.ResultReceiver
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
+import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
+import com.example.datasaverexampleapp.type_alias.AppString
+import com.google.android.exoplayer2.source.MediaSource
+import java.io.IOException
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
 
 class MediaPlayerPlaybackService  : MediaBrowserServiceCompat(), AudioManager.OnAudioFocusChangeListener
 {
+    private val TAG = javaClass.simpleName
+    private var mediaPlayer:MediaPlayer? = null
+    private var mediaSessionCompat: MediaSessionCompat? = null
+    private var mediaSource: Any? = null
+    private var isStreamingMedia = false
+    private lateinit var audioManager: AudioManager
+
+    override fun onCreate() {
+        super.onCreate()
+
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+        mediaSessionCompat = MediaSessionCompat(this,TAG).apply {
+
+            setCallback(object: MediaSessionCompat.Callback(){
+
+                override fun onPlay() {
+                    super.onPlay()
+
+                    mediaPlayer = null
+
+                    this@MediaPlayerPlaybackService.mediaSource?.let {
+
+                        when(it)
+                        {
+                            is String -> {
+//                                mediaPlayer = MediaPlayer.create(this@MediaPlayerPlaybackService,it)
+                                val mediaPlayer = MediaPlayer()
+                                mediaPlayer.setOnPreparedListener(object : MediaPlayer.OnPreparedListener{
+                                    override fun onPrepared(mp: MediaPlayer?) {
+                                        mediaPlayer.start()
+                                        Log.i("TAG","on Prepared")
+                                        mediaSessionCompat?.setPlaybackState(
+
+                                            PlaybackStateCompat.Builder()
+                                                .setActions( if (isStreamingMedia) PlaybackStateCompat.ACTION_PLAY_FROM_URI else PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                                .setState(PlaybackStateCompat.STATE_PLAYING, 0L, 0.0f)
+                                                .build()
+                                        )
+
+                                    }
+                                })
+
+                                try{
+                                    mediaPlayer.setDataSource(it)
+                                    mediaPlayer.prepareAsync()
+                                    isStreamingMedia = true
+                                }catch (e: IllegalArgumentException)
+                                {
+                                    e.printStackTrace()
+                                    Log.i("TAG","IllegalArgumentException: ${e.message}")
+                                    mediaSessionCompat?.setPlaybackState(
+                                        PlaybackStateCompat.Builder()
+                                            .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                            .setState(PlaybackStateCompat.STATE_ERROR, 0L, 0.0f)
+                                            .build()
+                                    )
+
+                                }catch (e:SecurityException)
+                                {
+                                    e.printStackTrace()
+                                    Log.i("TAG","SecurityException: ${e.message}")
+                                    mediaSessionCompat?.setPlaybackState(
+                                        PlaybackStateCompat.Builder()
+                                            .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                            .setState(PlaybackStateCompat.STATE_ERROR, 0L, 0.0f)
+                                            .build()
+                                    )
+                                }catch (e: IllegalStateException)
+                                {
+                                    e.printStackTrace()
+                                    Log.i("TAG","IllegalStateException: ${e.message}")
+                                    mediaSessionCompat?.setPlaybackState(
+                                        PlaybackStateCompat.Builder()
+                                            .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                            .setState(PlaybackStateCompat.STATE_ERROR, 0L, 0.0f)
+                                            .build()
+                                    )
+                                }catch (e: IOException)
+                                {
+                                    e.printStackTrace()
+                                    Log.i("TAG","IOException: ${e.message}")
+
+                                    mediaSessionCompat?.setPlaybackState(
+                                        PlaybackStateCompat.Builder()
+                                            .setActions(PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                            .setState(PlaybackStateCompat.STATE_ERROR, 0L, 0.0f)
+                                            .build()
+                                    )
+
+                                }
+                            }
+
+                            is Int -> {
+                                mediaPlayer = MediaPlayer.create(this@MediaPlayerPlaybackService,it)
+//                                mediaPlayer?.start()
+
+                                val result = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+                                {
+                                    val audioAttributes = AudioAttributes.Builder().setLegacyStreamType(AudioManager.STREAM_MUSIC).build()
+                                    val focusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                                        .setAudioAttributes(audioAttributes)
+                                        .setOnAudioFocusChangeListener(this@MediaPlayerPlaybackService)
+                                        .build()
+                                    audioManager.requestAudioFocus(focusRequest)
+                                } else {
+                                    audioManager.requestAudioFocus(this@MediaPlayerPlaybackService,AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN)
+                                }
+
+                                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+                                {
+                                    registerAudioBecomingNoisyReceiver()
+
+                                    mediaPlayer?.start()
+                                    // Call startService to keep your service alive during playback
+                                    startService(Intent(this@MediaPlayerPlaybackService,MediaPlayerPlaybackService::class.java))
+
+                                    mediaSessionCompat?.setPlaybackState(
+
+                                        PlaybackStateCompat.Builder()
+                                            .setActions( if (isStreamingMedia) PlaybackStateCompat.ACTION_PLAY_FROM_URI else PlaybackStateCompat.ACTION_PLAY_PAUSE)
+                                            .setState(PlaybackStateCompat.STATE_PLAYING, 0L, 0.0f)
+                                            .build()
+                                    )
+
+                                } else {}
+                            }
+
+                            else -> {}
+                        }
+                    }
+
+
+                }
+
+                override fun onStop() {
+                    super.onStop()
+
+                    mediaSessionCompat?.isActive = false
+                    mediaPlayer?.stop()
+
+                    mediaSessionCompat?.setPlaybackState(
+                        PlaybackStateCompat.Builder()
+                            .setActions(PlaybackStateCompat.ACTION_STOP)
+                            .setState(PlaybackStateCompat.STATE_STOPPED, 0L, 0.0f)
+                            .build()
+                    )
+
+                    stopSelf()
+                }
+
+                override fun onCommand(command: String?, extras: Bundle?, cb: ResultReceiver?) {
+
+                    command?.let {
+
+                        isStreamingMedia = false
+
+                        if (it == ADD_MEDIA_SOURCE)
+                        {
+                            extras?.let {
+                                mediaSource =  it.get(MEDIA_SOURCE)
+                                cb?.send(MEDIA_SOURCE_ADDED,null)
+                            }
+                        }
+                    }?: cb?.send(FAILED_ADDING_MEDIA_SOURCE,null)
+                }
+
+            })
+        }
+        sessionToken = mediaSessionCompat?.sessionToken
+    }
+
+    private val audioBecomeNoisyReceiver = object : BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        override fun onReceive(context: Context?, intent: Intent?) {
+            intent?.let { intentReceived ->
+                if (intentReceived.action == AudioManager.ACTION_AUDIO_BECOMING_NOISY) {
+                    Log.i(javaClass.simpleName, "action audio becoming noisy!")
+                }
+            }
+        }
+    }
+
+    private fun registerAudioBecomingNoisyReceiver() {
+        registerReceiver(
+            audioBecomeNoisyReceiver,
+            IntentFilter(AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+        )
+    }
+
     override fun onGetRoot(clientPackageName: String, clientUid: Int, rootHints: Bundle?): BrowserRoot? {
-        TODO("Not yet implemented")
+        // Returning null == no one can connect so we'll return something
+        return BrowserRoot(
+            getString(AppString.app_name), // Name visible in Android Auto
+            null // Bundle of optional extras
+        )
     }
 
     override fun onLoadChildren(parentId: String, result: Result<MutableList<MediaBrowserCompat.MediaItem>>) {
-        TODO("Not yet implemented")
+        // If you want to allow users to browse media content your app returns on Android Auto or Wear OS, return those results here.
+        result.sendResult(ArrayList())
     }
 
     override fun onAudioFocusChange(focusChange: Int) {
-        TODO("Not yet implemented")
+        Log.i("TAG", "focus change: ${focusChange}")
+    }
+
+    override fun onDestroy() {
+        mediaPlayer?.release()
+        mediaPlayer = null
+        super.onDestroy()
+        audioBecomeNoisyReceiver?.let {
+            try {
+                unregisterReceiver(it)
+            }catch (e:IllegalArgumentException)
+            {
+                e.printStackTrace()
+            }
+        }
     }
 
 }
