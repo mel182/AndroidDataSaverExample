cf4a0aef91d2fad66be87a533006ddd5b576b3fb
Melchior Vrolijk <vrol0004@gmail.com>
2021-06-01 23:03:02 +0200

    wifi p2p example added

--- app/src/main/java/com/example/datasaverexampleapp/wifi_p2p/Wifi_P2P_ExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/wifi_p2p/Wifi_P2P_ExampleActivity.kt
@@ -1,8 +1,11 @@
 package com.example.datasaverexampleapp.wifi_p2p
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.datasaverexampleapp.R
+import com.example.datasaverexampleapp.wifi_p2p.server.Wifi_P2P_ServerExampleActivity
+import kotlinx.android.synthetic.main.activity_wifi_p2_pexample.*
 
 class Wifi_P2P_ExampleActivity : AppCompatActivity()
 {
@@ -10,5 +13,10 @@ class Wifi_P2P_ExampleActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         title = "Wifi peer to peer Example"
         setContentView(R.layout.activity_wifi_p2_pexample)
+
+        wifi_p2p_server?.setOnClickListener {
+            val intent = Intent(this, Wifi_P2P_ServerExampleActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
