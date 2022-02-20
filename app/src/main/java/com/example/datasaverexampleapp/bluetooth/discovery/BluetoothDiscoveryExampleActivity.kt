414a86efd5ff5f60ceec23970121ee31727f40b8
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-20 21:53:19 +0100

    BluetoothDiscoveryExampleActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/bluetooth/discovery/BluetoothDiscoveryExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/bluetooth/discovery/BluetoothDiscoveryExampleActivity.kt
@@ -3,13 +3,13 @@ package com.example.datasaverexampleapp.bluetooth.discovery
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.bluetooth.base.BluetoothBaseActivity
+import com.example.datasaverexampleapp.databinding.ActivityBluetoothDiscoveryExampleBinding
 import com.example.datasaverexampleapp.type_alias.Drawable
 import com.example.datasaverexampleapp.type_alias.Layout
-import kotlinx.android.synthetic.main.activity_bluetooth_discovery_example.*
 
 /**
  * The process of two devices finding each other to connect is called 'discovery'. Before you can establish a
@@ -19,65 +19,71 @@ import kotlinx.android.synthetic.main.activity_bluetooth_discovery_example.*
 class BluetoothDiscoveryExampleActivity : BluetoothBaseActivity(Layout.activity_bluetooth_discovery_example)
 {
     private val devicesFound : ArrayList<DiscoveryDevice> = ArrayList()
+    private var binding: ActivityBluetoothDiscoveryExampleBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Bluetooth discovery Example"
 
-        bluetooth_action_button?.apply {
+        binding = DataBindingUtil.setContentView<ActivityBluetoothDiscoveryExampleBinding>(
+            this, Layout.activity_bluetooth_discovery_example
+        ).apply {
 
-            setOnClickListener {
+            bluetoothActionButton.apply {
 
-                when{
-                    this.text == "Enable Bluetooth" -> {
+                setOnClickListener {
 
-                        enabledBluetooth { enabled ->
+                    when{
+                        this.text == "Enable Bluetooth" -> {
 
-                            if (enabled)
-                            {
-                                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                                progress_status_text?.text = ""
-                                text = "Start discovery"
+                            enabledBluetooth { enabled ->
+
+                                if (enabled)
+                                {
+                                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_enabled)
+                                    progressStatusText.text = ""
+                                    text = "Start discovery"
+                                }
                             }
                         }
-                    }
 
-                    this.text == "Start discovery" -> {
-                        setScanMode()
-                        enabledDiscovery { enable ->
+                        this.text == "Start discovery" -> {
+                            setScanMode()
+                            enabledDiscovery { enable ->
 
-                            if (enable)
-                            {
-                                // Once a device has been made discoverable, it can then be discovered by another device.
-                                Toast.makeText(this@BluetoothDiscoveryExampleActivity,"discovery enabled",Toast.LENGTH_SHORT).show()
-                                startDiscovery()
-                            } else {
-                                Toast.makeText(this@BluetoothDiscoveryExampleActivity,"discovery failed",Toast.LENGTH_SHORT).show()
+                                if (enable)
+                                {
+                                    // Once a device has been made discoverable, it can then be discovered by another device.
+                                    Toast.makeText(this@BluetoothDiscoveryExampleActivity,"discovery enabled",Toast.LENGTH_SHORT).show()
+                                    startDiscovery()
+                                } else {
+                                    Toast.makeText(this@BluetoothDiscoveryExampleActivity,"discovery failed",Toast.LENGTH_SHORT).show()
+                                }
                             }
                         }
-                    }
 
-                    this.text == "Stop discovering" -> {
-                        progress_status_text?.text = "Stopping discovery...."
-                        cancelDiscovery()
+                        this.text == "Stop discovering" -> {
+                            progressStatusText.text = "Stopping discovery...."
+                            cancelDiscovery()
+                        }
                     }
+                }
+
+                if (!isBluetoothEnabled())
+                {
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_disabled)
+                    text = "Enable Bluetooth"
+                } else{
+                    bluetoothIcon?.setImageResource(Drawable.ic_bluetooth_enabled)
+                    text = "Start discovery"
+                    setScanMode()
+
                 }
             }
 
-            if (!isBluetoothEnabled())
-            {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_disabled)
-                text = "Enable Bluetooth"
-            } else{
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                text = "Start discovery"
-                setScanMode()
-
+            viewDiscoverDevicesButton.setOnClickListener {
+                DialogDeviceFoundResult(devicesFound).show(supportFragmentManager,null)
             }
-        }
-
-        view_discover_devices_button?.setOnClickListener {
-            DialogDeviceFoundResult(devicesFound).show(supportFragmentManager,null)
         }
     }
 
@@ -90,58 +96,68 @@ class BluetoothDiscoveryExampleActivity : BluetoothBaseActivity(Layout.activity_
     //       Device) while discovery is in progress.
 
         devicesFound.clear()
-        discover_device_layout?.visibility = View.INVISIBLE
-        progress_status_text?.text = "Starting discovery...."
-        startDiscovery { succeed ->
 
-            if (succeed)
-            {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                bluetooth_action_button?.text = "Stop discovering"
-                progress_status_text?.text = "Starting discovery...."
-                setScanMode()
-            } else {
+        binding?.apply {
 
-                if (!isBluetoothEnabled())
+            discoverDeviceLayout.visibility = View.INVISIBLE
+            progressStatusText.text = "Starting discovery...."
+            startDiscovery { succeed ->
+
+                if (succeed)
                 {
-                    bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_disabled)
-                    bluetooth_action_button?.text = "Enable Bluetooth"
-                } else{
-                    bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                    bluetooth_action_button?.text = "Start discovery"
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_enabled)
+                    bluetoothActionButton.text = "Stop discovering"
+                    progressStatusText.text = "Starting discovery...."
                     setScanMode()
-                }
-                progress_status_text?.text = ""
-            }
+                } else {
 
+                    if (!isBluetoothEnabled())
+                    {
+                        bluetoothIcon.setImageResource(Drawable.ic_bluetooth_disabled)
+                        bluetoothActionButton.text = "Enable Bluetooth"
+                    } else{
+                        bluetoothIcon.setImageResource(Drawable.ic_bluetooth_enabled)
+                        bluetoothActionButton.text = "Start discovery"
+                        setScanMode()
+                    }
+                    progressStatusText.text = ""
+                }
+            }
         }
     }
 
     private fun setScanMode()
     {
-        if (isBluetoothEnabled())
-        {
-            getBluetoothScanMode { mode ->
-                scan_mode?.text = if (mode == -1) "-" else mode.asScanModeText()
+        binding?.apply {
+            if (isBluetoothEnabled())
+            {
+                getBluetoothScanMode { mode ->
+                    scanMode.text = if (mode == -1) "-" else mode.asScanModeText()
+                }
+            } else {
+                scanMode.text = "-"
             }
-        } else {
-            scan_mode?.text = "-"
         }
     }
 
     override fun onBluetoothDiscoveryStarted() {
         super.onBluetoothDiscoveryStarted()
-        progress_status_text?.text = "Discovering...."
-        bluetooth_action_button?.text = "Stop discovering"
+        binding?.apply {
+            progressStatusText.text = "Discovering...."
+            bluetoothActionButton.text = "Stop discovering"
+        }
+
     }
 
     override fun onBluetoothDiscoveryEnded() {
         super.onBluetoothDiscoveryEnded()
-        progress_status_text?.text = ""
-        bluetooth_action_button?.text = "Start discovery"
-        discover_device_layout?.visibility = View.VISIBLE
-        devices_text?.text = "${devicesFound.size} device(s) found"
-        view_discover_devices_button?.isEnabled = devicesFound.isNotEmpty()
+        binding?.apply {
+            progressStatusText.text = ""
+            bluetoothActionButton.text = "Start discovery"
+            discoverDeviceLayout.visibility = View.VISIBLE
+            devicesText.text = "${devicesFound.size} device(s) found"
+            viewDiscoverDevicesButton.isEnabled = devicesFound.isNotEmpty()
+        }
     }
 
     override fun onBluetoothRemoteDeviceFound(name: String?, device: BluetoothDevice?) {
@@ -152,26 +168,29 @@ class BluetoothDiscoveryExampleActivity : BluetoothBaseActivity(Layout.activity_
     override fun onBluetoothStateChanged(state: Int) {
         super.onBluetoothStateChanged(state)
 
-        when(state)
-        {
-            BluetoothAdapter.STATE_OFF -> {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_disabled)
-                progress_status_text?.text = ""
-            }
+        binding?.apply {
 
-            BluetoothAdapter.STATE_ON -> {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                progress_status_text?.text = ""
-            }
+            when(state)
+            {
+                BluetoothAdapter.STATE_OFF -> {
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_disabled)
+                    progressStatusText.text = ""
+                }
 
-            BluetoothAdapter.STATE_TURNING_OFF -> {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_enabled)
-                progress_status_text?.text = "Turning off..."
-            }
+                BluetoothAdapter.STATE_ON -> {
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_enabled)
+                    progressStatusText.text = ""
+                }
 
-            BluetoothAdapter.STATE_TURNING_ON -> {
-                bluetooth_icon?.setImageResource(Drawable.ic_bluetooth_disabled)
-                progress_status_text?.text = "Turning on..."
+                BluetoothAdapter.STATE_TURNING_OFF -> {
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_enabled)
+                    progressStatusText.text = "Turning off..."
+                }
+
+                BluetoothAdapter.STATE_TURNING_ON -> {
+                    bluetoothIcon.setImageResource(Drawable.ic_bluetooth_disabled)
+                    progressStatusText.text = "Turning on..."
+                }
             }
         }
     }
