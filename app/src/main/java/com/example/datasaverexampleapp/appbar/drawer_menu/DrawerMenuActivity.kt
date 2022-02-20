0f3b045f4c95fccaabe8cac8df63ffd4d65abbe1
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-20 21:33:32 +0100

    AppBarToolbarOffscreenActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/appbar/drawer_menu/DrawerMenuActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/appbar/drawer_menu/DrawerMenuActivity.kt
@@ -6,11 +6,13 @@ import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.datasaverexampleapp.R
-import kotlinx.android.synthetic.main.item_drawer_layout.*
+import com.example.datasaverexampleapp.databinding.ActivityDrawerMenuBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 
 class DrawerMenuActivity : AppCompatActivity() {
 
@@ -25,54 +27,59 @@ class DrawerMenuActivity : AppCompatActivity() {
         // Ensure that navigation button is visible
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        drawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.cardinal_east, R.string.cardinal_north)
-        drawer_layout?.addDrawerListener(object : DrawerLayout.DrawerListener{
-            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+        DataBindingUtil.setContentView<ActivityDrawerMenuBinding>(
+            this, Layout.activity_drawer_menu
+        ).apply {
 
-            override fun onDrawerOpened(drawerView: View) {}
+            drawerToggle = ActionBarDrawerToggle(this@DrawerMenuActivity, drawerItem.drawerLayout, R.string.cardinal_east, R.string.cardinal_north)
+            drawerItem.drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener{
+                override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
 
-            override fun onDrawerClosed(drawerView: View) {
+                override fun onDrawerOpened(drawerView: View) {}
 
-                var newFragment:Fragment? = null
+                override fun onDrawerClosed(drawerView: View) {
 
-                when(selectedItem)
-                {
-                    R.id.nav_home -> {
-                        newFragment = HomeFragment()
-                        supportActionBar?.title = this@DrawerMenuActivity.title
+                    var newFragment:Fragment? = null
+
+                    when(selectedItem)
+                    {
+                        R.id.nav_home -> {
+                            newFragment = HomeFragment()
+                            supportActionBar?.title = this@DrawerMenuActivity.title
+                        }
+
+                        R.id.nav_account -> {
+                            newFragment = AccountFragment()
+                            supportActionBar?.title = this@DrawerMenuActivity.title
+                        }
+
+                        R.id.nav_about -> {
+                            newFragment = AboutFragment()
+                            supportActionBar?.title = this@DrawerMenuActivity.title
+                        }
                     }
 
-                    R.id.nav_account -> {
-                        newFragment = AccountFragment()
-                        supportActionBar?.title = this@DrawerMenuActivity.title
+                    newFragment?.apply {
+
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.drawer_content_view, this)
+                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                            .commit()
                     }
 
-                    R.id.nav_about -> {
-                        newFragment = AboutFragment()
-                        supportActionBar?.title = this@DrawerMenuActivity.title
-                    }
+                    selectedItem = 0
                 }
 
-                newFragment?.apply {
+                override fun onDrawerStateChanged(newState: Int) {}
+            })
 
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.drawer_content_view, this)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                        .commit()
-                }
-
-                selectedItem = 0
+            drawerItem.navView.setNavigationItemSelectedListener {
+                selectedItem = it.itemId
+                it.isChecked = true
+                title = it.title.toString()
+                drawerItem.drawerLayout.closeDrawer(drawerItem.navView)
+                true
             }
-
-            override fun onDrawerStateChanged(newState: Int) {}
-        })
-
-        nav_view?.setNavigationItemSelectedListener {
-            selectedItem = it.itemId
-            it.isChecked = true
-            title = it.title.toString()
-            drawer_layout.closeDrawer(nav_view)
-            true
         }
 
         supportFragmentManager.beginTransaction()
