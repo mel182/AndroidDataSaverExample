de6287e72f92561d9d7cf5a9649a96f6d662e3aa
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-20 23:36:47 +0100

    OptionDialogFragment synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/hardware_sensor/OptionDialogFragment.kt
+++ app/src/main/java/com/example/datasaverexampleapp/hardware_sensor/OptionDialogFragment.kt
@@ -6,18 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
+import com.example.datasaverexampleapp.databinding.ItemSensorOptionsListBinding
 import com.example.datasaverexampleapp.hardware_sensor.book_example.CompassFinalWithSensorActivity
 import com.example.datasaverexampleapp.hardware_sensor.book_example.DeviceOrientationActivity
 import com.example.datasaverexampleapp.hardware_sensor.book_example.ForceMeterActivity
 import com.example.datasaverexampleapp.hardware_sensor.book_example.user_activity_recognition.UserRecognitionActivity
 import com.example.datasaverexampleapp.hardware_sensor.general.HardwareSensorActivity
 import com.example.datasaverexampleapp.type_alias.Layout
-import kotlinx.android.synthetic.main.item_sensor_options_list.*
-import kotlinx.android.synthetic.main.item_sensor_options_list.view.*
 
 class OptionDialogFragment(private val activity:AppCompatActivity) : DialogFragment(), View.OnClickListener
 {
+    private var binding: ItemSensorOptionsListBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,51 +30,58 @@ class OptionDialogFragment(private val activity:AppCompatActivity) : DialogFragm
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        view.examples_list_button?.setOnClickListener(this)
-        view.force_meter_example_button?.setOnClickListener(this)
-        view.compass_artificial_horizon_button?.setOnClickListener(this)
-        view.user_recognition_button?.setOnClickListener(this)
-        view.device_orientation_button?.setOnClickListener(this)
-
-    }
-
-    override fun onClick(v: View?) {
-        v?.let { viewClicked ->
-
-            when(viewClicked.id)
-            {
-                examples_list_button.id -> {
-                    val intent = Intent(activity, HardwareSensorActivity::class.java)
-                    startActivity(intent)
-                    dismiss()
-                }
-
-                force_meter_example_button.id -> {
-                    val intent = Intent(activity, ForceMeterActivity::class.java)
-                    startActivity(intent)
-                    dismiss()
-                }
-
-                device_orientation_button.id -> {
-                    val intent = Intent(activity, DeviceOrientationActivity::class.java)
-                    startActivity(intent)
-                    dismiss()
-                }
-
-                compass_artificial_horizon_button.id -> {
-                    val intent = Intent(activity, CompassFinalWithSensorActivity::class.java)
-                    startActivity(intent)
-                    dismiss()
-                }
-
-                user_recognition_button.id -> {
-                    val intent = Intent(activity, UserRecognitionActivity::class.java)
-                    startActivity(intent)
-                    dismiss()
-                }
+        activity.apply {
+            binding= DataBindingUtil.setContentView<ItemSensorOptionsListBinding>(
+                this, Layout.item_sensor_options_list
+            ).apply {
+                examplesListButton.setOnClickListener(this@OptionDialogFragment)
+                forceMeterExampleButton.setOnClickListener(this@OptionDialogFragment)
+                compassArtificialHorizonButton.setOnClickListener(this@OptionDialogFragment)
+                userRecognitionButton.setOnClickListener(this@OptionDialogFragment)
+                deviceOrientationButton.setOnClickListener(this@OptionDialogFragment)
             }
         }
     }
 
+    override fun onClick(v: View?) {
 
+        binding?.apply {
+
+            v?.let { viewClicked ->
+
+                when(viewClicked.id)
+                {
+                    examplesListButton.id -> {
+                        val intent = Intent(activity, HardwareSensorActivity::class.java)
+                        startActivity(intent)
+                        dismiss()
+                    }
+
+                    forceMeterExampleButton.id -> {
+                        val intent = Intent(activity, ForceMeterActivity::class.java)
+                        startActivity(intent)
+                        dismiss()
+                    }
+
+                    deviceOrientationButton.id -> {
+                        val intent = Intent(activity, DeviceOrientationActivity::class.java)
+                        startActivity(intent)
+                        dismiss()
+                    }
+
+                    compassArtificialHorizonButton.id -> {
+                        val intent = Intent(activity, CompassFinalWithSensorActivity::class.java)
+                        startActivity(intent)
+                        dismiss()
+                    }
+
+                    userRecognitionButton.id -> {
+                        val intent = Intent(activity, UserRecognitionActivity::class.java)
+                        startActivity(intent)
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
 }
