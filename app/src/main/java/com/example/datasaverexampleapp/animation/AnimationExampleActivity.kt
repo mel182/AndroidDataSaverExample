ad222d0a06302e8cbb898b23d76af2a0524d0ddb
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-20 20:33:00 +0100

    AnimationExampleActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/animation/AnimationExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/animation/AnimationExampleActivity.kt
@@ -5,15 +5,17 @@ import android.animation.AnimatorInflater
 import android.content.Intent
 import android.graphics.drawable.AnimationDrawable
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.R
 import com.example.datasaverexampleapp.animation.activity_transition.ActivityTransition1
-import kotlinx.android.synthetic.main.activity_animation_example.*
+import com.example.datasaverexampleapp.databinding.ActivityAnimationExampleBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 import kotlinx.coroutines.*
 import kotlin.math.hypot
 
@@ -24,83 +26,88 @@ class AnimationExampleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animation_example)
 
-        alpha_0_1_example_textview?.apply {
-            val alphaAnimator = AnimatorInflater.loadAnimator(this@AnimationExampleActivity, R.animator.set_alpha_between_0_1)
-            alphaAnimator.setTarget(this)
-            alphaAnimator.start()
-        }
+        DataBindingUtil.setContentView<ActivityAnimationExampleBinding>(
+            this, Layout.activity_animation_example
+        ).apply {
 
-        group_animation_example?.apply {
-
-            setOnClickListener {
-                val rotationShrinkAnimation = AnimationUtils.loadAnimation(this@AnimationExampleActivity, R.anim.rotation_shrink)
-                startAnimation(rotationShrinkAnimation)
+            alpha01ExampleTextview.apply {
+                val alphaAnimator = AnimatorInflater.loadAnimator(this@AnimationExampleActivity, R.animator.set_alpha_between_0_1)
+                alphaAnimator.setTarget(this)
+                alphaAnimator.start()
             }
-        }
 
-        imageViewAnimation?.apply {
-            setBackgroundResource(R.drawable.image_animation)
-            val animationDrawable = background as AnimationDrawable
-            animationDrawable.start()
-        }
+            groupAnimationExample.apply {
 
-        CoroutineScope(Dispatchers.IO).launch {
-            val job: Job = launch(Dispatchers.IO){
-
-                launch(Dispatchers.Main){
-
-                    Toast.makeText(this@AnimationExampleActivity, "dispatcher main", Toast.LENGTH_SHORT).show()
-                    delay(100)
+                setOnClickListener {
+                    val rotationShrinkAnimation = AnimationUtils.loadAnimation(this@AnimationExampleActivity, R.anim.rotation_shrink)
+                    startAnimation(rotationShrinkAnimation)
                 }
             }
-        }
 
-        circular_reveal_animation_button?.setOnClickListener {
+            imageViewAnimation?.apply {
+                setBackgroundResource(R.drawable.image_animation)
+                val animationDrawable = background as AnimationDrawable
+                animationDrawable.start()
+            }
 
-            val centerX = circular_reveal_textView.width / 2
-            val centerY = circular_reveal_textView.height / 2
-            val coveringRadius = hypot(centerX.toDouble(), centerY.toDouble()).toFloat()
+            CoroutineScope(Dispatchers.IO).launch {
+                val job: Job = launch(Dispatchers.IO){
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    launch(Dispatchers.Main){
 
-                if (circular_reveal_textView.visibility == View.INVISIBLE)
-                {
-                    val anim = ViewAnimationUtils.createCircularReveal(circular_reveal_textView, centerX,centerY,0F,coveringRadius)
-                    circular_reveal_textView.visibility = View.VISIBLE
-                    anim.start()
-                    anim.addListener(object: Animator.AnimatorListener {
-                        override fun onAnimationStart(animation: Animator?) {
-
-                        }
-
-                        override fun onAnimationEnd(animation: Animator?) {
-                            circular_reveal_animation_button?.text = "Circular reveal hide"
-                        }
-
-                        override fun onAnimationCancel(animation: Animator?) {}
-                        override fun onAnimationRepeat(animation: Animator?) {}
-                    })
-                } else {
-                    val anim = ViewAnimationUtils.createCircularReveal(circular_reveal_textView, centerX,centerY,coveringRadius,0F)
-                    anim.start()
-                    anim.addListener(object: Animator.AnimatorListener {
-                        override fun onAnimationStart(animation: Animator?) {}
-
-                        override fun onAnimationEnd(animation: Animator?) {
-                            circular_reveal_textView.visibility = View.INVISIBLE
-                            circular_reveal_animation_button?.text = "Circular reveal"
-                        }
-
-                        override fun onAnimationCancel(animation: Animator?) {}
-                        override fun onAnimationRepeat(animation: Animator?) {}
-                    })
+                        Toast.makeText(this@AnimationExampleActivity, "dispatcher main", Toast.LENGTH_SHORT).show()
+                        delay(100)
+                    }
                 }
             }
-        }
 
-        activity_transition_animation_button?.setOnClickListener {
-            val intent = Intent(this,ActivityTransition1::class.java)
-            startActivity(intent)
+            circularRevealAnimationButton.setOnClickListener {
+
+                val centerX =  circularRevealTextView.width / 2
+                val centerY = circularRevealTextView.height / 2
+                val coveringRadius = hypot(centerX.toDouble(), centerY.toDouble()).toFloat()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                    if (circularRevealTextView.visibility == View.INVISIBLE)
+                    {
+                        val anim = ViewAnimationUtils.createCircularReveal(circularRevealTextView, centerX,centerY,0F,coveringRadius)
+                        circularRevealTextView.visibility = View.VISIBLE
+                        anim.start()
+                        anim.addListener(object: Animator.AnimatorListener {
+                            override fun onAnimationStart(animation: Animator?) {
+
+                            }
+
+                            override fun onAnimationEnd(animation: Animator?) {
+                                circularRevealAnimationButton.text = "Circular reveal hide"
+                            }
+
+                            override fun onAnimationCancel(animation: Animator?) {}
+                            override fun onAnimationRepeat(animation: Animator?) {}
+                        })
+                    } else {
+                        val anim = ViewAnimationUtils.createCircularReveal(circularRevealTextView, centerX,centerY,coveringRadius,0F)
+                        anim.start()
+                        anim.addListener(object: Animator.AnimatorListener {
+                            override fun onAnimationStart(animation: Animator?) {}
+
+                            override fun onAnimationEnd(animation: Animator?) {
+                                circularRevealTextView.visibility = View.INVISIBLE
+                                circularRevealAnimationButton.text = "Circular reveal"
+                            }
+
+                            override fun onAnimationCancel(animation: Animator?) {}
+                            override fun onAnimationRepeat(animation: Animator?) {}
+                        })
+                    }
+                }
+            }
+
+            activityTransitionAnimationButton.setOnClickListener {
+                val intent = Intent(this@AnimationExampleActivity,ActivityTransition1::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
