8d9c70d0be37f2c2a2ff536a8b79c9d47f438df0
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-20 20:25:31 +0100

    ScrollViewActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/ScrollViewActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/ScrollViewActivity.kt
@@ -4,30 +4,35 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_scroll_view.*
+import androidx.databinding.DataBindingUtil
+import com.example.datasaverexampleapp.databinding.ActivityScrollViewBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 
 class ScrollViewActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scroll_view)
 
-       scroller?.onScroll { _, _ ->
-           val scrollBounds = Rect()
-           scroller?.getDrawingRect(scrollBounds)
+        DataBindingUtil.setContentView<ActivityScrollViewBinding>(this, Layout.activity_scroll_view).apply {
 
-           val top = imageView2.y
-           val bottom = top + imageView2.height
+            scroller?.onScroll { _, _ ->
+                val scrollBounds = Rect()
+                scroller?.getDrawingRect(scrollBounds)
 
-           if (scrollBounds.top < top && scrollBounds.bottom > bottom)
-           {
-               top_section.visibility = View.GONE
+                val top = imageView2.y
+                val bottom = top + imageView2.height
+
+                if (scrollBounds.top < top && scrollBounds.bottom > bottom)
+                {
+                    topSection.visibility = View.GONE
 //               Log.i("TAG","Image view 2 visible")
-           } else {
-               top_section.visibility = View.VISIBLE
+                } else {
+                    topSection.visibility = View.VISIBLE
 //               Log.i("TAG", "Image view 2 not visible")
-           }
-       }
-
+                }
+            }
+        }
 
 //        scroller?.viewTreeObserver?.addOnScrollChangedListener {
 //
