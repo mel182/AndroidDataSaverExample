26d90526e82f2fcd4ee86972e8a87d540644d74c
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 11:27:41 +0100

    ImplicitDelegationActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/implicit_delegation_example/ImplicitDelegationActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/implicit_delegation_example/ImplicitDelegationActivity.kt
@@ -2,9 +2,11 @@ package com.example.datasaverexampleapp.implicit_delegation_example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.datasaverexampleapp.R
-import kotlinx.android.synthetic.main.activity_implicit_delegation.*
+import com.example.datasaverexampleapp.databinding.ActivityImplicitDelegationBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 
 class ImplicitDelegationActivity : AppCompatActivity() {
 
@@ -13,16 +15,20 @@ class ImplicitDelegationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_implicit_delegation)
         title = "OnCLick Implicit delegation example"
 
-        list?.apply {
-            layoutManager = LinearLayoutManager(this@ImplicitDelegationActivity)
-            adapter = ImplicitItemAdapter(arrayListOf(
-                "Value 1","Value 2",
-                "Value 3","Value 4",
-                "Value 5","Value 6",
-                "Value 7","Value 8",
-                "Value 9","Value 10",
-                "Value 11","Value 12",
-                "Value 13","Value 14"))
+        DataBindingUtil.setContentView<ActivityImplicitDelegationBinding>(
+            this, Layout.activity_implicit_delegation
+        ).apply {
+            list.apply {
+                layoutManager = LinearLayoutManager(this@ImplicitDelegationActivity)
+                adapter = ImplicitItemAdapter(arrayListOf(
+                    "Value 1","Value 2",
+                    "Value 3","Value 4",
+                    "Value 5","Value 6",
+                    "Value 7","Value 8",
+                    "Value 9","Value 10",
+                    "Value 11","Value 12",
+                    "Value 13","Value 14"))
+            }
         }
     }
 }
