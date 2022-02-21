ba392636759d499a62b51006cc8546fceb694509
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 13:20:11 +0100

    MediaSessionActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/video_audio/media_session/MediaSessionActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/media_session/MediaSessionActivity.kt
@@ -7,7 +7,9 @@ import android.support.v4.media.session.MediaSessionCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.R
+import com.example.datasaverexampleapp.databinding.ActivityMediaSessionBinding
 import com.example.datasaverexampleapp.type_alias.Layout
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
@@ -15,7 +17,6 @@ import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.upstream.RawResourceDataSource
 import com.google.android.exoplayer2.util.Util
-import kotlinx.android.synthetic.main.activity_exo_player.*
 
 /**
  * This is a media session example using exo player
@@ -33,64 +34,70 @@ class MediaSessionActivity : AppCompatActivity()
     // 'MediaSessionCompat' class from within the 'onCreate' method of your
     // Activity, passing in a 'Context' and a 'String' for logging error messages.
     private var mediaSession : MediaSessionCompat? = null
+    private var binding: ActivityMediaSessionBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(Layout.activity_media_session)
         title = "Media session Example"
 
-        mediaSession = MediaSessionCompat(applicationContext, "TAG")
-        // Create a new Exo Player
-        player = ExoPlayer.Builder(this).build()
+        binding = DataBindingUtil.setContentView<ActivityMediaSessionBinding>(
+            this, Layout.activity_media_session
+        ).apply {
 
-        // To receive media control from devices such as Bluetooth headsets, Wear OS,
-        // and Android Auto, you must then call 'setFlag', indicating that you wish
-        // the Media Session to handle media buttons and transport controls.
-        mediaSession?.setFlags(
-            MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
-                    MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
-        )
+            mediaSession = MediaSessionCompat(applicationContext, "TAG")
+            // Create a new Exo Player
+            player = ExoPlayer.Builder(this@MediaSessionActivity).build()
 
-        // associate the ExoPlayer with the player View
-        player_view?.player = player
-
-        //Build a DataSource.Factory capable of loading http and local content
-        val dataSourceFactory = DefaultDataSourceFactory(
-            this, Util.getUserAgent(
-                this,
-                getString(R.string.app_name)
+            // To receive media control from devices such as Bluetooth headsets, Wear OS,
+            // and Android Auto, you must then call 'setFlag', indicating that you wish
+            // the Media Session to handle media buttons and transport controls.
+            mediaSession?.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS or
+                        MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
             )
-        )
 
-        // This is the MediaSource representing the media to be played.
-        val rawDirectoryResource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
-            .createMediaSource(RawResourceDataSource.buildRawResourceUri(R.raw.bon_vibe))
+            // associate the ExoPlayer with the player View
+            playerView.player = player
 
-        // Start loading the media source
-        player?.prepare(rawDirectoryResource)
+            //Build a DataSource.Factory capable of loading http and local content
+            val dataSourceFactory = DefaultDataSourceFactory(
+                this@MediaSessionActivity, Util.getUserAgent(
+                    this@MediaSessionActivity,
+                    getString(R.string.app_name)
+                )
+            )
 
-        // The final step is to create and set an instance of the
-        // 'MediaSessionCompat.Callback' class. The callback methods
-        // you implement within this class will receive the media button
-        // requests and allow you to respond to them appropriately.
-        mediaSession?.setCallback( object : MediaSessionCompat.Callback(){
+            // This is the MediaSource representing the media to be played.
+            val rawDirectoryResource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(RawResourceDataSource.buildRawResourceUri(R.raw.bon_vibe))
 
-            override fun onPlay() {
-                Toast.makeText(applicationContext, "onPlay pressed!",Toast.LENGTH_SHORT).show()
-                player?.playWhenReady
-            }
+            // Start loading the media source
+            player?.prepare(rawDirectoryResource)
 
-            override fun onPause() {
-                Toast.makeText(applicationContext, "onPause pressed!",Toast.LENGTH_SHORT).show()
-            }
+            // The final step is to create and set an instance of the
+            // 'MediaSessionCompat.Callback' class. The callback methods
+            // you implement within this class will receive the media button
+            // requests and allow you to respond to them appropriately.
+            mediaSession?.setCallback( object : MediaSessionCompat.Callback(){
 
-            override fun onSeekTo(pos: Long) {
-                Toast.makeText(applicationContext, "onSeekTo pressed!",Toast.LENGTH_SHORT).show()
-                player?.seekTo(pos)
-            }
-        })
+                override fun onPlay() {
+                    Toast.makeText(applicationContext, "onPlay pressed!",Toast.LENGTH_SHORT).show()
+                    player?.playWhenReady
+                }
 
-        updatePlaybackState()
+                override fun onPause() {
+                    Toast.makeText(applicationContext, "onPause pressed!",Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onSeekTo(pos: Long) {
+                    Toast.makeText(applicationContext, "onSeekTo pressed!",Toast.LENGTH_SHORT).show()
+                    player?.seekTo(pos)
+                }
+            })
+
+            updatePlaybackState()
+        }
     }
 
     private fun updatePlaybackState()
@@ -114,7 +121,7 @@ class MediaSessionActivity : AppCompatActivity()
     }
 
     override fun onStop() {
-        player_view?.player = null
+        binding?.playerView?.player = null
         player?.release()
         player = null
         super.onStop()
