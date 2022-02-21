c32375b1a34e90c8b33c675c0ad4954026e5e1c4
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 12:34:46 +0100

    StorageManagerExampleActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/storage_manager/StorageManagerExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/storage_manager/StorageManagerExampleActivity.kt
@@ -13,13 +13,15 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ShareCompat
 import androidx.core.content.FileProvider
+import androidx.databinding.DataBindingUtil
 import androidx.documentfile.provider.DocumentFile
 import com.example.datasaverexampleapp.BuildConfig
 import com.example.datasaverexampleapp.R
+import com.example.datasaverexampleapp.databinding.ActivityStorageManagerExampleBinding
 import com.example.datasaverexampleapp.handlers.activity_result_handler.constant.ActivityIntent
 import com.example.datasaverexampleapp.handlers.activity_result_handler.interfaces.OnActivityResultCallback
 import com.example.datasaverexampleapp.intent_example.IntentBaseActivity
-import kotlinx.android.synthetic.main.activity_storage_manager_example.*
+import com.example.datasaverexampleapp.type_alias.Layout
 import java.io.*
 
 class StorageManagerExampleActivity : IntentBaseActivity() {
@@ -30,110 +32,113 @@ class StorageManagerExampleActivity : IntentBaseActivity() {
         setContentView(R.layout.activity_storage_manager_example)
         this.title = "Storage Manager Example"
 
-        open_directory_example?.setOnClickListener {
+        DataBindingUtil.setContentView<ActivityStorageManagerExampleBinding>(
+            this, Layout.activity_storage_manager_example
+        ).apply {
+            openDirectoryExample.setOnClickListener {
 
-            /*
-            Allow the user to pick a directory subtree.
-            When invoked, the system will display the various DocumentsProvider instances installed on the device,
-            letting the user navigate through them.
-            Apps can fully manage documents within the returned directory.
-            */
-            activityForResult(
-                ActivityIntent.PICK_OPEN_DIRECTORY,
-                object : OnActivityResultCallback {
+                /*
+                Allow the user to pick a directory subtree.
+                When invoked, the system will display the various DocumentsProvider instances installed on the device,
+                letting the user navigate through them.
+                Apps can fully manage documents within the returned directory.
+                */
+                activityForResult(
+                    ActivityIntent.PICK_OPEN_DIRECTORY,
+                    object : OnActivityResultCallback {
 
-                    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-                    override fun onActivityResult(resultCode: Int, data: Intent?) {
+                        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                        override fun onActivityResult(resultCode: Int, data: Intent?) {
 
-                        if (resultCode == RESULT_OK) {
-                            data?.let {
-                                Toast.makeText(
-                                    this@StorageManagerExampleActivity,
-                                    "document tree: ${it.data}",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                Log.i("TAG", "Data content uri: ${it.data}")
+                            if (resultCode == RESULT_OK) {
+                                data?.let {
+                                    Toast.makeText(
+                                        this@StorageManagerExampleActivity,
+                                        "document tree: ${it.data}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.i("TAG", "Data content uri: ${it.data}")
 
-                                it.data?.let {
-                                    val rawUri = it.toString()
-                                    handleDocumentTreeUri(Uri.parse(rawUri))
+                                    it.data?.let {
+                                        val rawUri = it.toString()
+                                        handleDocumentTreeUri(Uri.parse(rawUri))
+                                    }
                                 }
+                            } else if (resultCode == RESULT_CANCELED) {
+                                Log.i("TAG", "Result cancelled")
                             }
-                        } else if (resultCode == RESULT_CANCELED) {
-                            Log.i("TAG", "Result cancelled")
                         }
                     }
+                )
+            }
+
+            fileProviderExample.setOnClickListener {
+
+                val photoDirectory = File(filesDir, "images")
+                val imageToShare = File(photoDirectory, "shared_image.png")
+
+                val contentUri = FileProvider.getUriForFile(
+                    this@StorageManagerExampleActivity,
+                    BuildConfig.APPLICATION_ID + ".files",
+                    imageToShare
+                )
+
+                ShareCompat.IntentBuilder.from(this@StorageManagerExampleActivity).setType("image/png").setStream(contentUri).startChooser()
+            }
+
+            receiveFileFromFileProviderExample.setOnClickListener {
+
+                val uri = ShareCompat.IntentReader.from(this@StorageManagerExampleActivity).stream
+                var bitmap:Bitmap? = null
+                try {
+                    uri?.let {
+                        val inputStream = contentResolver.openInputStream(it)
+                        bitmap = BitmapFactory.decodeStream(inputStream)
+                        Log.i("TAG", "Bitmap: ${bitmap}")
+                    }
+                }catch (e: IOException)
+                {
+                    Log.i("TAG", "IO Exception: ${e.message}")
                 }
-            )
-        }
+            }
 
-        file_provider_example?.setOnClickListener {
+            temporaryFileAccessExample.setOnClickListener {
 
-            val photoDirectory = File(filesDir, "images")
-            val imageToShare = File(photoDirectory, "shared_image.png")
+                activityForResult(
+                    ActivityIntent.PICK_IMAGE_FILE_INTENT,
+                    object : OnActivityResultCallback {
 
-            val contentUri = FileProvider.getUriForFile(
-                this,
-                BuildConfig.APPLICATION_ID + ".files",
-                imageToShare
-            )
+                        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+                        override fun onActivityResult(resultCode: Int, data: Intent?) {
 
-            ShareCompat.IntentBuilder.from(this).setType("image/png").setStream(contentUri).startChooser()
-        }
+                            if (resultCode == RESULT_OK) {
+                                data?.let {
+                                    Toast.makeText(
+                                        this@StorageManagerExampleActivity,
+                                        "image file data: ${it.data}",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    Log.i("TAG", "Data content uri: ${it.data}")
 
-        receive_file_from_file_provider_example?.setOnClickListener {
+                                    it.data?.let {
 
-            val uri = ShareCompat.IntentReader.from(this).stream
-            var bitmap:Bitmap? = null
-            try {
-                uri?.let {
-                    val inputStream = contentResolver.openInputStream(it)
-                    bitmap = BitmapFactory.decodeStream(inputStream)
-                    Log.i("TAG", "Bitmap: ${bitmap}")
-                }
-            }catch (e: IOException)
-            {
-                Log.i("TAG", "IO Exception: ${e.message}")
+                                        val sendIntent = Intent()
+                                        sendIntent.action = Intent.ACTION_VIEW
+                                        sendIntent.type = "image/png"
+                                        sendIntent.data = it
+                                        sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                                        startActivity(sendIntent)
+
+                                    }
+                                }
+                            } else if (resultCode == RESULT_CANCELED) {
+                                Log.i("TAG", "Image file result cancelled")
+                            }
+                        }
+                    }
+                )
             }
         }
-
-        temporary_file_access_example?.setOnClickListener {
-
-            activityForResult(
-                ActivityIntent.PICK_IMAGE_FILE_INTENT,
-                object : OnActivityResultCallback {
-
-                    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-                    override fun onActivityResult(resultCode: Int, data: Intent?) {
-
-                        if (resultCode == RESULT_OK) {
-                            data?.let {
-                                Toast.makeText(
-                                    this@StorageManagerExampleActivity,
-                                    "image file data: ${it.data}",
-                                    Toast.LENGTH_SHORT
-                                ).show()
-                                Log.i("TAG", "Data content uri: ${it.data}")
-
-                                it.data?.let {
-
-                                    val sendIntent = Intent()
-                                    sendIntent.action = Intent.ACTION_VIEW
-                                    sendIntent.type = "image/png"
-                                    sendIntent.data = it
-                                    sendIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                                    startActivity(sendIntent)
-
-                                }
-                            }
-                        } else if (resultCode == RESULT_CANCELED) {
-                            Log.i("TAG", "Image file result cancelled")
-                        }
-                    }
-                }
-            )
-        }
-
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
