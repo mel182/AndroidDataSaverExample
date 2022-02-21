75fd325ac502065b9155e936a64c3a8bcf9b4335
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 12:27:27 +0100

    SnackBarActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/snackbar/SnackBarActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/snackbar/SnackBarActivity.kt
@@ -1,12 +1,14 @@
 package com.example.datasaverexampleapp.snackbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.R
+import com.example.datasaverexampleapp.databinding.ActivitySnackBarBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_snack_bar.*
 
 class SnackBarActivity : AppCompatActivity()
 {
@@ -14,26 +16,31 @@ class SnackBarActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_snack_bar)
 
-        // Note: When a Snackbar is used with a coordinator layout users are able to swipe it away and any FloatingActionButton that would
-        // potentially overlap the incoming Snackbar will be smoothly animated alongside the Snackbar
-        show_button?.setOnClickListener {
+        DataBindingUtil.setContentView<ActivitySnackBarBinding>(
+            this, Layout.activity_snack_bar
+        ).apply {
 
-            val snackBar = Snackbar.make(root_view,"Snackbar", Snackbar.LENGTH_LONG)
+            // Note: When a Snackbar is used with a coordinator layout users are able to swipe it away and any FloatingActionButton that would
+            // potentially overlap the incoming Snackbar will be smoothly animated alongside the Snackbar
+            showButton.setOnClickListener {
 
-            snackBar.setAction("Undo", object : View.OnClickListener{
-                override fun onClick(v: View?)
+                val snackBar = Snackbar.make(rootView,"Snackbar", Snackbar.LENGTH_LONG)
+
+                snackBar.setAction("Undo", object : View.OnClickListener{
+                    override fun onClick(v: View?)
+                    {
+                        Snackbar.make(rootView,"Undo button clicked!", Snackbar.LENGTH_LONG).show()
+                    }
+                })
+
+                snackBar.addCallback( object: Snackbar.Callback()
                 {
-                    Snackbar.make(root_view,"Undo button clicked!", Snackbar.LENGTH_LONG).show()
-                }
-            })
-
-            snackBar.addCallback( object: Snackbar.Callback()
-            {
-                override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
-                    Toast.makeText(this@SnackBarActivity, "Snackbar dismissed",Toast.LENGTH_SHORT).show()
-                }
-            })
-            snackBar.show()
+                    override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                        Toast.makeText(this@SnackBarActivity, "Snackbar dismissed",Toast.LENGTH_SHORT).show()
+                    }
+                })
+                snackBar.show()
+            }
         }
     }
 }
