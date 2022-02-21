44cf5db742cbd7f3aec889e197429fee34778c8b
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 16:51:11 +0100

    Wifi_P2P_ServerExampleActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/wifi_p2p/server/Wifi_P2P_ServerExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/wifi_p2p/server/Wifi_P2P_ServerExampleActivity.kt
@@ -3,15 +3,16 @@
 package com.example.datasaverexampleapp.wifi_p2p.server
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.R
-import kotlinx.android.synthetic.main.activity_wifi_p2_pserver_example.*
+import com.example.datasaverexampleapp.databinding.ActivityWifiP2PserverExampleBinding
+import com.example.datasaverexampleapp.type_alias.Layout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.io.IOException
 import java.net.ServerSocket
 import java.net.Socket
@@ -28,35 +29,38 @@ class Wifi_P2P_ServerExampleActivity : AppCompatActivity() {
         setContentView(R.layout.activity_wifi_p2_pserver_example)
         title = "Wifi peer to peer server"
 
-        start_wifi_p2p_host_button?.apply {
+        DataBindingUtil.setContentView<ActivityWifiP2PserverExampleBinding>(
+            this, Layout.activity_wifi_p2_pserver_example
+        ).apply {
+            startWifiP2pHostButton.apply {
 
+                setOnClickListener {
 
-            setOnClickListener {
+                    when(this.text.toString().lowercase(Locale.ROOT))
+                    {
+                        "start" -> {
+                            wifiP2pServerHostStatus.text = "Starting...."
 
-                when(this.text.toString().lowercase(Locale.ROOT))
-                {
-                    "start" -> {
-                        wifi_p2p_server_host_status?.text = "Starting...."
-
-                        try {
-                            val serverSocket = ServerSocket(port)
-                            wifi_p2p_server_host_status?.text = "Hosting on port: $port"
-                            start_wifi_p2p_host_button?.text = "stop"
-                            CoroutineScope(Dispatchers.IO).launch {
-                                serverClient = serverSocket.accept()
+                            try {
+                                val serverSocket = ServerSocket(port)
+                                wifiP2pServerHostStatus.text = "Hosting on port: $port"
+                                startWifiP2pHostButton.text = "stop"
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    serverClient = serverSocket.accept()
+                                }
+                            } catch (e: IOException) {
+                                Log.i("TAG","Error starting server: ${e.message}")
                             }
-                        } catch (e: IOException) {
-                            Log.i("TAG","Error starting server: ${e.message}")
                         }
-                    }
 
-                    "stop" -> {
-                        serverClient?.let {
-                            if (!it.isClosed)
-                                it.close()
+                        "stop" -> {
+                            serverClient?.let {
+                                if (!it.isClosed)
+                                    it.close()
 
-                            wifi_p2p_server_host_status?.text = "-"
-                            start_wifi_p2p_host_button?.text = "start"
+                                wifiP2pServerHostStatus.text = "-"
+                                startWifiP2pHostButton.text = "start"
+                            }
                         }
                     }
                 }
