8dd4d3e279e854ccfa9772d9a790ca6ad11797a5
Melchior Vrolijk <vrol0004@gmail.com>
2022-02-21 10:55:30 +0100

    UserRecognitionActivity synthetic removed

--- app/src/main/java/com/example/datasaverexampleapp/hardware_sensor/book_example/user_activity_recognition/UserRecognitionActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/hardware_sensor/book_example/user_activity_recognition/UserRecognitionActivity.kt
@@ -5,12 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.datasaverexampleapp.base_classes.BaseActivity
+import com.example.datasaverexampleapp.databinding.ActivityUserRecognizationBinding
 import com.example.datasaverexampleapp.type_alias.Drawable
 import com.example.datasaverexampleapp.type_alias.Layout
 import com.google.android.gms.location.DetectedActivity
-import kotlinx.android.synthetic.main.activity_user_recognization.*
+
 // Google Activity recognition API enabled you to understand what activity your users are performing
 // in the physical world. By periodically analyzing short burst of data received from device
 // Sensors, Activity Recognition attempts to detect what activities the user is performing, including
@@ -19,6 +21,7 @@ class UserRecognitionActivity : BaseActivity(Layout.activity_user_recognization)
 
     private var broadcastReceiver : BroadcastReceiver? = null
     private var activityConfidence by ConfidenceDelegate()
+    private var binding: ActivityUserRecognizationBinding? = null
 
     // You must include the ACTIVITY_RECOGNITION permission in your manifest
     //
@@ -30,6 +33,9 @@ class UserRecognitionActivity : BaseActivity(Layout.activity_user_recognization)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "User Recognition Example"
+        binding = DataBindingUtil.setContentView<ActivityUserRecognizationBinding>(
+            this, Layout.activity_user_recognization
+        )
         proceedWithInitialization()
     }
 
@@ -48,7 +54,7 @@ class UserRecognitionActivity : BaseActivity(Layout.activity_user_recognization)
                     } else if (userActivityIntent.action == Constant.SERVICE_STATE)
                     {
                         userActivityIntent.getStringExtra("state")?.apply {
-                            state_textView?.text = this
+                            binding?.stateTextView?.text = this
                         }
                     }
                 }
@@ -82,35 +88,36 @@ class UserRecognitionActivity : BaseActivity(Layout.activity_user_recognization)
 
     private fun handleUserActivity(type:Int, confidence:Int)
     {
-
         // For any Detected Activity, use the 'getType' and 'getConfidence' methods
         // to find the type of activity detected and the percent confidence in that result
         activityConfidence = confidence.toString()
-        user_activity_textView?.text = when(type)
-        {
-            DetectedActivity.IN_VEHICLE -> "In vehicle (${activityConfidence})"
-            DetectedActivity.ON_BICYCLE -> "On bicycle (${activityConfidence})"
-            DetectedActivity.ON_FOOT -> "On foot (${activityConfidence})"
-            DetectedActivity.RUNNING -> "Running (${activityConfidence})"
-            DetectedActivity.STILL -> "Still (${activityConfidence})"
-            DetectedActivity.TILTING -> "Tilting (${activityConfidence})"
-            DetectedActivity.WALKING -> "Walking (${activityConfidence})"
-            else -> "Unknown (${activityConfidence})"
-        }
+        binding?.apply {
+            userActivityTextView.text = when(type)
+            {
+                DetectedActivity.IN_VEHICLE -> "In vehicle (${activityConfidence})"
+                DetectedActivity.ON_BICYCLE -> "On bicycle (${activityConfidence})"
+                DetectedActivity.ON_FOOT -> "On foot (${activityConfidence})"
+                DetectedActivity.RUNNING -> "Running (${activityConfidence})"
+                DetectedActivity.STILL -> "Still (${activityConfidence})"
+                DetectedActivity.TILTING -> "Tilting (${activityConfidence})"
+                DetectedActivity.WALKING -> "Walking (${activityConfidence})"
+                else -> "Unknown (${activityConfidence})"
+            }
 
-        val icon = when(type)
-        {
-            DetectedActivity.IN_VEHICLE -> Drawable.ic_driving
-            DetectedActivity.ON_BICYCLE -> Drawable.ic_bicycle
-            DetectedActivity.ON_FOOT -> Drawable.ic_walking
-            DetectedActivity.RUNNING -> Drawable.ic_running
-            DetectedActivity.STILL -> Drawable.ic_still
-            DetectedActivity.TILTING -> Drawable.ic_tilting
-            DetectedActivity.WALKING -> Drawable.ic_walking
-            else -> Drawable.ic_unknown
-        }
+            val icon = when(type)
+            {
+                DetectedActivity.IN_VEHICLE -> Drawable.ic_driving
+                DetectedActivity.ON_BICYCLE -> Drawable.ic_bicycle
+                DetectedActivity.ON_FOOT -> Drawable.ic_walking
+                DetectedActivity.RUNNING -> Drawable.ic_running
+                DetectedActivity.STILL -> Drawable.ic_still
+                DetectedActivity.TILTING -> Drawable.ic_tilting
+                DetectedActivity.WALKING -> Drawable.ic_walking
+                else -> Drawable.ic_unknown
+            }
 
-        activity_icon?.setImageResource(icon)
+            activityIcon.setImageResource(icon)
+        }
     }
 
     private fun startTracking()
