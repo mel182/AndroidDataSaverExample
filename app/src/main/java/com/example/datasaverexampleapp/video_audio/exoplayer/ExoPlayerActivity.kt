a52c76968722f93ee3f62670786332da7194b85c
mel182 <vrol0004@gmail.com>
2022-06-21 13:30:36 +0200

    exo player additional modification added

--- app/src/main/java/com/example/datasaverexampleapp/video_audio/exoplayer/ExoPlayerActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/video_audio/exoplayer/ExoPlayerActivity.kt
@@ -2,16 +2,15 @@
 
 package com.example.datasaverexampleapp.video_audio.exoplayer
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.datasaverexampleapp.R
 import com.example.datasaverexampleapp.databinding.ActivityExoPlayerBinding
 import com.example.datasaverexampleapp.type_alias.Layout
-import com.google.android.exoplayer2.DefaultLoadControl
-import com.google.android.exoplayer2.DefaultRenderersFactory
-import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
@@ -43,7 +42,7 @@ import com.google.android.exoplayer2.util.Util
  * fast forward, rewind and a seek bar for skipping through the video, and
  * which can be added to your Activity or Fragment layout.
  */
-class ExoPlayerActivity : AppCompatActivity()
+class ExoPlayerActivity : AppCompatActivity(), Player.Listener
 {
     private var player:ExoPlayer? = null
     private var binding: ActivityExoPlayerBinding? = null
@@ -89,6 +88,8 @@ class ExoPlayerActivity : AppCompatActivity()
                 .setLoadControl(DefaultLoadControl())
                 .build()
 
+            player?.addListener(this)
+
             // associate the ExoPlayer with the player View
             binding?.playerView?.player = player
 
@@ -105,17 +106,78 @@ class ExoPlayerActivity : AppCompatActivity()
                 )
 
                 // This is the MediaSource representing the media to be played.
-                val mediaItem = MediaItem.fromUri(RawResourceDataSource.buildRawResourceUri(R.raw.sample_video))
-                //val mediaItem = MediaItem.fromUri(Uri.parse("https://objectstore.true.nl/voetbalinternational:video/2022/carr_nistelrooij_ajax.mp4"))
-                val rawDirectoryResource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
-                    .createMediaSource(mediaItem)
+//                val mediaItem = MediaItem.fromUri(RawResourceDataSource.buildRawResourceUri(R.raw.sample_video))
+                //3968e3af-cc38-41c3-b19e-b72bdd272c26
+                val mediaItem = MediaItem.Builder().setUri("https://objectstore.true.nl/voetbalinternational:video/2022/carr_nistelrooij_ajax.mp4")
+                    .setMediaId("3968e3af-cc38-41c3-b19e-b72bdd272c26")
+                    .setAdsConfiguration(MediaItem.AdsConfiguration.Builder(Uri.parse("https://hbopenbid.pubmatic.com/translator?source=prebid-client")).build())
+                    .build()
+//                val mediaItem = MediaItem.fromUri(Uri.parse("https://objectstore.true.nl/voetbalinternational:video/2022/carr_nistelrooij_ajax.mp4"))
+                val mediaItem2 = MediaItem.Builder().setUri("https://objectstore.true.nl/voetbalinternational:video/2022/carr_pijlers_vannistelrooij.mp4")
+                    .setAdsConfiguration(MediaItem.AdsConfiguration.Builder(Uri.parse("https://ib.adnxs.com/ut/v3/prebid")).build())
+                    .setMediaId("d86a443c-4237-435f-895b-f31ae5025c3c")
+                    .build()
+                //val mediaItem2 = MediaItem.fromUri(Uri.parse("https://objectstore.true.nl/voetbalinternational:video/2022/carr_pijlers_vannistelrooij.mp4"))
 
-                setMediaSource(rawDirectoryResource)
+                /*
+                MediaItem mediaItem =
+    new MediaItem.Builder()
+        .setUri(videoUri)
+        .setAdsConfiguration(
+            new MediaItem.AdsConfiguration.Builder(adTagUri).build())
+        .build();
+                */
+
+                // This is used when using exoplayer playlist
+                addMediaItem(mediaItem)
+                addMediaItem(mediaItem2)
+
+                // If you want to use progressive media source
+//                val rawDirectoryResource: MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory!!)
+//                    .createMediaSource(mediaItem)
+//
+//                setMediaSource(rawDirectoryResource)
+
                 prepare()
+
+                // Enable this code when using playlist
+                play()
             }
         }
     }
 
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+
+        when(playbackState) {
+
+            Player.STATE_BUFFERING -> {
+                Log.i("TAG45","state buffering")
+            }
+
+            Player.STATE_ENDED -> {
+                Log.i("TAG45","state ended")
+                player?.seekToNextMediaItem()
+            }
+
+            Player.STATE_IDLE -> {
+                Log.i("TAG45","state idle")
+            }
+
+            Player.STATE_READY -> {
+                Log.i("TAG45","state ready")
+            }
+
+            else -> {
+                Log.i("TAG45","state else statement")
+            }
+        }
+    }
+
+    override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
+        super.onMediaItemTransition(mediaItem, reason)
+        Log.i("TAG45","onMediaItemTransition: ${mediaItem?.mediaId}")
+    }
 
     override fun onStop() {
         binding?.playerView?.player = null
