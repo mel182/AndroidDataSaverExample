e4617dcdb136a8d6c435fdea2299667ac745ada5
mel182 <vrol0004@gmail.com>
2022-08-29 17:55:01 +0200

    textfield, button and snackbar example added

--- app/src/main/java/com/example/datasaverexampleapp/compose/textfield_buttons_snackbar_example/TextFieldButtonSnackbarActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/compose/textfield_buttons_snackbar_example/TextFieldButtonSnackbarActivity.kt
@@ -1,16 +1,16 @@
 package com.example.datasaverexampleapp.compose.textfield_buttons_snackbar_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
+import androidx.appcompat.app.AppCompatActivity
+import com.example.datasaverexampleapp.compose.textfield_buttons_snackbar_example.ui.TextFieldButtonAndSnackBarActivityView
 
 class TextFieldButtonSnackbarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         title = "Textfield Button and Snackbar compose example"
-        setContent { 
-            Text(text = "Test")
+        setContent {
+            TextFieldButtonAndSnackBarActivityView()
         }
     }
 }
