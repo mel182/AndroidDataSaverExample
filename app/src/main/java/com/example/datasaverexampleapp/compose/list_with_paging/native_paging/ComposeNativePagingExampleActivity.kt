61c37f100f445de645d1a3183f5a79fb03f354da
mel182 <vrol0004@gmail.com>
2022-09-20 12:07:24 +0200

    swipe refresh jetpack compose example added

--- app/src/main/java/com/example/datasaverexampleapp/compose/list_with_paging/native_paging/ComposeNativePagingExampleActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/compose/list_with_paging/native_paging/ComposeNativePagingExampleActivity.kt
@@ -4,17 +4,27 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.example.datasaverexampleapp.compose.extensions.toPx
 import com.example.datasaverexampleapp.paging.PagingViewModel
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.SwipeRefreshState
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 class ComposeNativePagingExampleActivity : AppCompatActivity() {
 
@@ -27,50 +37,65 @@ class ComposeNativePagingExampleActivity : AppCompatActivity() {
 
         setContent {
 
-            val pagingItemList = pagingItemViewModel.pagingItemsList.collectAsLazyPagingItems()
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            Column(modifier = Modifier.fillMaxWidth()) {
 
-                val itemCount = pagingItemList.itemCount
-                var lastAgeGroup: Int? = null
+                var isRefreshing by remember { mutableStateOf(false) }
+                val swipeRefreshState = rememberSwipeRefreshState(isRefreshing = isRefreshing)
+                val pagingItemList = pagingItemViewModel.pagingItemsList.collectAsLazyPagingItems()
 
-                for (index in 0 until itemCount) {
+                SwipeRefresh(state = swipeRefreshState, onRefresh = {
+                    isRefreshing = true
+                    pagingItemList.refresh()
+                }, indicator = { state, trigger ->
+                    CustomIndicator(state,trigger)
+                }) {
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
 
-                    pagingItemList.peek(index)?.let { nextItem ->
-                        val age = nextItem.age
-                        // create a header for every 10y of age
-                        if (lastAgeGroup == null || age > lastAgeGroup!!) {
-                            lastAgeGroup = (lastAgeGroup ?: 0) + 10
-                            val ageGroup = lastAgeGroup
-                            stickyHeader {
-                                Text(text = "<= $ageGroup", modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(Color.LightGray)
-                                    .padding(16.dp))
-                            }
+                        val itemCount = pagingItemList.itemCount
+                        var lastAgeGroup: Int? = null
+
+                        if (isRefreshing) {
+                            isRefreshing = false
                         }
 
-                        pagingItemList[index]?.let { listItem ->
-                            item(key = listItem.name) {
+                        for (index in 0 until itemCount) {
 
-                                Column(modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(16.dp)
-                                ) {
-                                    Text(
-                                        text = listItem.name,
-                                        fontSize = 20.sp,
-                                        color = Color.Black
-                                    )
-                                    Spacer(modifier = Modifier.height(8.dp))
-                                    Text(text = listItem.age.toString())
+                            pagingItemList.peek(index)?.let { nextItem ->
+                                val age = nextItem.age
+                                // create a header for every 10y of age
+                                if (lastAgeGroup == null || age > lastAgeGroup!!) {
+                                    lastAgeGroup = (lastAgeGroup ?: 0) + 10
+                                    val ageGroup = lastAgeGroup
+                                    stickyHeader {
+                                        Text(text = "<= $ageGroup", modifier = Modifier
+                                            .fillMaxWidth()
+                                            .background(Color.LightGray)
+                                            .padding(16.dp))
+                                    }
+                                }
+
+                                pagingItemList[index]?.let { listItem ->
+                                    item(key = listItem.name) {
+
+                                        Column(modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(16.dp)
+                                        ) {
+                                            Text(
+                                                text = listItem.name,
+                                                fontSize = 20.sp,
+                                                color = Color.Black
+                                            )
+                                            Spacer(modifier = Modifier.height(8.dp))
+                                            Text(text = listItem.age.toString())
+                                        }
+                                    }
                                 }
                             }
                         }
-                    }
-                }
 
 
-                // the following code code is without sticky header only native paging
+                        // the following code code is without sticky header only native paging
 //                items(pagingItemList) { item ->
 //                    item?.let {
 //                        Column(modifier = Modifier
@@ -111,7 +136,53 @@ class ComposeNativePagingExampleActivity : AppCompatActivity() {
 //                        }
 //                    }
 //                }
+                    }
+                }
             }
         }
     }
+}
+
+@Composable
+fun CustomIndicator(swipeRefreshState: SwipeRefreshState, refreshTriggerDistance: Dp, color: Color = Color.LightGray) {
+
+    if (swipeRefreshState.isRefreshing) {
+        Text(text = "Test", modifier = Modifier.fillMaxWidth())
+    }
+
+
+
+//    Box(modifier = Modifier
+//        .drawWithCache {
+//            onDrawBehind {
+//                val distance = refreshTriggerDistance.toPx()
+//                val progress = (swipeRefreshState.indicatorOffset / distance).coerceIn(0f, 1f)
+//                val brush = Brush.verticalGradient(
+//                    0f to color.copy(alpha = 0.45f),
+//                    1f to color.copy(alpha = 0f)
+//                )
+//
+//                drawRect(brush = brush, alpha = FastOutSlowInEasing.transform(progress))
+//            }
+//        }
+//        .fillMaxWidth()
+//        .height(80.dp)) {
+//
+//        if (swipeRefreshState.isRefreshing) {
+//            LinearProgressIndicator(modifier = Modifier.fillMaxWidth(), color = Color.Blue)
+//        } else {
+//            val trigger = refreshTriggerDistance.toPx()
+//            val progress = (swipeRefreshState.indicatorOffset / trigger).coerceIn(0f,1f)
+//            LinearProgressIndicator(
+//                progress = progress,
+//                modifier = Modifier.fillMaxWidth(),
+//                color = Color.Green
+//            )
+//        }
+//    }
+
+
+
+
+
 }
