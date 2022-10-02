adbccfbd22fdbbfc158113cf10a8b296bcd9deac
mel182 <vrol0004@gmail.com>
2022-10-02 21:08:02 +0200

    rectangle rotate with animation example added

---
+++ app/src/main/java/com/example/datasaverexampleapp/compose/path_transformations_and_clipping/RotateRectangleWithAnimationExampleActivity.kt
@@ -0,0 +1,51 @@
+package com.example.datasaverexampleapp.compose.path_transformations_and_clipping
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.rotate
+
+class RotateRectangleWithAnimationExampleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        title = "Rotate rectangle with animation example"
+
+        setContent {
+
+            // This example shows how to rotate an rectangle 45 degrees with an animation duration of 3 sec.
+            val animateDegrees = remember {
+                Animatable(initialValue = 0f)
+            }
+
+            LaunchedEffect(key1 = true) {
+                animateDegrees.animateTo(
+                    targetValue = 45f,
+                    animationSpec = tween(
+                        durationMillis = 3000
+                    )
+                )
+            }
+
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                rotate(degrees = 45f, pivot = Offset(200f,200f)) {
+                    drawRect(
+                        color = Color.Red,
+                        topLeft = Offset(x =100f,y = 100f),
+                        size = Size(width = 200f, height = 200f)
+                    )
+                }
+            }
+        }
+    }
+}
