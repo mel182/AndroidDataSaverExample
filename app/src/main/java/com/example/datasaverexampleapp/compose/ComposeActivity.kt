d626ed1e9aa9f6a3c4f081721edcae7d9b201005
mel182 <vrol0004@gmail.com>
2022-10-02 09:28:00 +0200

    path operations example added

--- app/src/main/java/com/example/datasaverexampleapp/compose/ComposeActivity.kt
+++ app/src/main/java/com/example/datasaverexampleapp/compose/ComposeActivity.kt
@@ -15,6 +15,7 @@ import com.example.datasaverexampleapp.compose.navigation.ComposeNavigationActiv
 import com.example.datasaverexampleapp.compose.path.PathBasicActivity
 import com.example.datasaverexampleapp.compose.path.PathStrokeJoinExampleActivity
 import com.example.datasaverexampleapp.compose.path.QuadraticCubicBezierPathExampleActivity
+import com.example.datasaverexampleapp.compose.path_operations.PathOperationsExampleActivity
 import com.example.datasaverexampleapp.compose.row_and_column_example.RowAndColumnComposeActivity
 import com.example.datasaverexampleapp.compose.screen_orientation.ScreenOrientationActivity
 import com.example.datasaverexampleapp.compose.simple_list.ComposeSimpleListActivity
@@ -132,9 +133,11 @@ class ComposeActivity : AppCompatActivity() {
                 startActivity(intent)
             }
 
+            pathOperationsExample.setOnClickListener {
+                val intent = Intent(this@ComposeActivity, PathOperationsExampleActivity::class.java)
+                startActivity(intent)
+            }
 
-
-//            PathStrokeJoinExampleActivity
         }
     }
 }
