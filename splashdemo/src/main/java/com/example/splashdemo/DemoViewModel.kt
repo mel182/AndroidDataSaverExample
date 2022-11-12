731585f681b8c6b71324f9246ad1e34839fc366b
Melchior Vrolijk <vrol0004@gmail.com>
2022-11-12 22:16:12 +0100

    first static splash screen implemented

---
+++ splashdemo/src/main/java/com/example/splashdemo/DemoViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.splashdemo
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class DemoViewModel : ViewModel()
+{
+    private val _isLoading = MutableStateFlow(true)
+    val isLoading = _isLoading.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(2000)
+            _isLoading.value = false
+        }
+    }
+}
--- splashdemo/src/main/java/com/example/splashdemo/MainActivity.kt
+++ splashdemo/src/main/java/com/example/splashdemo/MainActivity.kt
@@ -3,41 +3,38 @@ package com.example.splashdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.splashdemo.ui.theme.DataSaverExampleAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity()
+{
+    private val demoViewModel: DemoViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                demoViewModel.isLoading.value
+            }
+        }
+
         setContent {
             DataSaverExampleAppTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Box(modifier = Modifier.fillMaxSize().background(color = Color.White),
+                    contentAlignment = Alignment.Center) {
+                    Text(text = "Main screen")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DataSaverExampleAppTheme {
-        Greeting("Android")
     }
 }
