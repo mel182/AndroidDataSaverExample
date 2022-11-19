b5a78520222b06f063b91f09899eca3b6b4b9d50
Melchior Vrolijk <vrol0004@gmail.com>
2022-11-19 16:12:32 +0100

    navigation compose example module added

---
+++ navigationcompose/src/main/java/com/example/navigationcompose/NavigationMainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.navigationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigationcompose.ui.theme.DataSaverExampleAppTheme
+
+class NavigationMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DataSaverExampleAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DataSaverExampleAppTheme {
+        Greeting("Android")
+    }
+}
