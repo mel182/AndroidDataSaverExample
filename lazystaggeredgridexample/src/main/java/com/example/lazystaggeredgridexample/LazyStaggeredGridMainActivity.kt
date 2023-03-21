8bd4c9c442550da3442cbe50b7bde7ae98c0cb1d
Melchior Vrolijk <vrol0004@gmail.com>
2023-03-21 22:24:20 +0100

    lazy staggered grid example added

---
+++ lazystaggeredgridexample/src/main/java/com/example/lazystaggeredgridexample/LazyStaggeredGridMainActivity.kt
@@ -0,0 +1,67 @@
+@file:OptIn(ExperimentalFoundationApi::class)
+
+package com.example.lazystaggeredgridexample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lazystaggeredgridexample.models.StaggeredListItem
+import com.example.lazystaggeredgridexample.ui.theme.DataSaverExampleAppTheme
+import kotlin.random.Random
+
+class LazyStaggeredGridMainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DataSaverExampleAppTheme {
+                
+                val dummyData = (1..100).map {
+                    StaggeredListItem(
+                        height = Random.nextInt(100,300).dp,
+                        color = Color(Random.nextLong(0xFFFFFFFF)).copy(alpha = 1f)
+                    )
+                }
+
+                LazyVerticalStaggeredGrid(
+                    columns = StaggeredGridCells.Fixed(2),
+                    modifier = Modifier.fillMaxSize(),
+                    contentPadding = PaddingValues(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalItemSpacing = 16.dp
+                ) {
+                    items(dummyData) { item ->
+                        RandomColorBox(item = item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RandomColorBox(item:StaggeredListItem) {
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(item.height)
+        .clip(RoundedCornerShape(10.dp))
+        .background(item.color)
+    )
+
+}
---
+++ lazystaggeredgridexample/src/main/java/com/example/lazystaggeredgridexample/models/ListItem.kt
@@ -0,0 +1,6 @@
+package com.example.lazystaggeredgridexample.models
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+data class StaggeredListItem(val height: Dp, val color:Color)
