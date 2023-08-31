9d88221bbbd4c8cc5a3eb8db7098146fd49db851
Melchior Vrolijk <vrol0004@gmail.com>
2023-08-31 22:45:58 +0200

    gradle compose version update to all modules and material 3 text field in progress

---
+++ material3textfield/src/main/java/com/http/material3textfield/Material3TextField.kt
@@ -0,0 +1,46 @@
+package com.http.material3textfield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.http.material3textfield.ui.theme.DataSaverExampleAppTheme
+
+class Material3TextField : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DataSaverExampleAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DataSaverExampleAppTheme {
+        Greeting("Android")
+    }
+}
