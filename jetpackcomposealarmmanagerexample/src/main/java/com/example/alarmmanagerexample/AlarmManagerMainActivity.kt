268d96153eb88cd77dcc544f5f2decb0ac62de7b
Melchior Vrolijk <vrol0004@gmail.com>
2023-09-10 13:28:21 +0200

    jetpack compose alarm manager example

--- jetpackcomposealarmmanagerexample/src/main/java/com/example/alarmmanagerexample/AlarmManagerMainActivity.kt
+++ jetpackcomposealarmmanagerexample/src/main/java/com/example/alarmmanagerexample/AlarmManagerMainActivity.kt
@@ -5,7 +5,14 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
