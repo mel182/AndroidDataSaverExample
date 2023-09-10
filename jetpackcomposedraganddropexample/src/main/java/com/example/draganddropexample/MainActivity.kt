837e9c48c3dd459dfd77a3ed35abe0608f34579b
Melchior Vrolijk <vrol0004@gmail.com>
2023-09-10 13:12:44 +0200

    jetpack compose drag and drop example

--- jetpackcomposedraganddropexample/src/main/java/com/example/draganddropexample/MainActivity.kt
+++ jetpackcomposedraganddropexample/src/main/java/com/example/draganddropexample/MainActivity.kt
@@ -12,7 +12,13 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
