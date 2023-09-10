20fbeed036f3204ef4d1f36afb6798f325f682f4
Melchior Vrolijk <vrol0004@gmail.com>
2023-09-10 12:03:13 +0200

    jetpack compose webview navigation example

--- jetpackcomposewebviewnavigation/src/main/java/com/example/webviewnavigation/WebviewNavigationMainActivity.kt
+++ jetpackcomposewebviewnavigation/src/main/java/com/example/webviewnavigation/WebviewNavigationMainActivity.kt
@@ -12,20 +12,23 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.webviewnavigation.delegates.topRowHeightDelegate
-import com.example.webviewnavigation.ui.theme.DataSaverExampleAppTheme
 
 class WebviewNavigationMainActivity : ComponentActivity() {
 
