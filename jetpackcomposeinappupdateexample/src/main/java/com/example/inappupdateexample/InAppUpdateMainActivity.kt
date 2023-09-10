95ffb6a6c7d8a6d8d4114e6075470e15cf2a2d6d
Melchior Vrolijk <vrol0004@gmail.com>
2023-09-10 11:40:28 +0200

    in app update example renamed to jetpack compose in app update example

--- jetpackcomposeinappupdateexample/src/main/java/com/example/inappupdateexample/InAppUpdateMainActivity.kt
+++ jetpackcomposeinappupdateexample/src/main/java/com/example/inappupdateexample/InAppUpdateMainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
