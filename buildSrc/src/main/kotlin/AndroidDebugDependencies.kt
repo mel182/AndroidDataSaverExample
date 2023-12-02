0bf851bd97e1176e05e02f4befa0e2b1d7c80e41
Melchior Vrolijk <vrol0004@gmail.com>
2023-12-02 23:04:54 +0100

    code cleanup

--- buildSrc/src/main/kotlin/AndroidDebugDependencies.kt
+++
@@ -1,8 +0,0 @@
-interface AndroidDebugDependencies {
-
-    val composeUiToolingDebug
-        get() = "androidx.compose.ui:ui-tooling:${Versions.composeUIVersion}"
-
-    val composeUiTestManifestDebug
-        get() = "androidx.compose.ui:ui-test-manifest:${Versions.composeUIVersion}"
-}
