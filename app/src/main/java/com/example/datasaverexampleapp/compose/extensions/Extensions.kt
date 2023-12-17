46c52cb1e2ba2c8f5697c7a28e5b240c492b734e
Melchior Vrolijk <vrol0004@gmail.com>
2023-12-17 21:46:28 +0100

    code cleanup

--- app/src/main/java/com/example/datasaverexampleapp/compose/extensions/Extensions.kt
+++
@@ -1,8 +0,0 @@
-package com.example.datasaverexampleapp.compose.extensions
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.Dp
-
-@Composable
-fun Dp.toPx() : Float = with(LocalDensity.current) { this@toPx.toPx() }
