1cb879b28b2e5b21063feae41b6093fce3fb5d94
Melchior Vrolijk <vrol0004@gmail.com>
2023-12-17 21:42:42 +0100

    Jetpack Compose weight scale removed from main app

--- app/src/main/java/com/example/datasaverexampleapp/compose/weight_scale/ui/LineType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.datasaverexampleapp.compose.weight_scale.ui
-
-sealed class LineType {
-    object Normal : LineType()
-    object FiveStep : LineType()
-    object TenStep : LineType()
-}
