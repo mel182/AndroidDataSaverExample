dec038e816a2c5aee99911d0c77f491999d174f7
Melchior Vrolijk <vrol0004@gmail.com>
2024-04-01 23:50:06 +0200

    circular indicator work in progress

---
+++ jetpackcomposecircularindicatordraggable/src/main/java/com/jetpackcompose/circularindicatordraggable/CustomCircularProgressIndicator.kt
@@ -0,0 +1,180 @@
+package com.jetpackcompose.circularindicatordraggable
+
+import android.graphics.Paint
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+@Composable
+fun CustomCircularProgressIndicator(
+    modifier: Modifier = Modifier,
+    initialValue: Int,
+    primaryColor: Color,
+    secondaryColor: Color,
+    minValue: Int = 0,
+    maxValue: Int = 100,
+    valueUnit: String = "",
+    circularRadius: Float,
+    onPositionChange: (Int) -> Unit
+) {
+
+    var circleCenter by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    var positionValue by remember {
+        mutableStateOf(initialValue)
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+
+            val width = size.width
+            val height = size.height
+            val circleThickness = width / 25f
+            circleCenter = Offset(x = width / 2f, y = height / 2f)
+
+            drawCircle(
+                brush = Brush.radialGradient(
+                    colors =
+                    listOf(
+                        primaryColor.copy(0.45f),
+                        secondaryColor.copy(0.15f)
+                    )
+                ),
+                radius = circularRadius,
+                center = circleCenter
+            )
+
+            drawCircle(
+                style = Stroke(
+                    width = circleThickness
+                ),
+                color = secondaryColor,
+                radius = circularRadius,
+                center = circleCenter
+            )
+
+            drawArc(
+                color = primaryColor,
+                startAngle = 90f,
+                sweepAngle = (360f / maxValue) * positionValue.toFloat(),
+                style = Stroke(
+                    width = circleThickness,
+                    cap = StrokeCap.Round
+                ),
+                useCenter = false,
+                size = Size(
+                    width = circularRadius * 2f,
+                    height = circularRadius * 2f
+                ),
+                topLeft = Offset(
+                    x = (width - circularRadius * 2f) / 2f,
+                    y = (height - circularRadius * 2f) / 2f
+                )
+            )
+
+            val outerRadius = circularRadius + circleThickness / 2f
+            val gap = 15f
+            for (value in 0..(maxValue - minValue)) {
+                val color =
+                    if (value < positionValue - minValue) primaryColor else primaryColor.copy(alpha = 0.3f)
+                val angleInDegrees = value * 360f / (maxValue - minValue).toFloat()
+                val angleInRadius = angleInDegrees * PI / 180f + PI / 2f
+
+                val yGapAdjustment = cos(x = angleInDegrees * PI / 180f) * gap
+                val xGapAdjustment = -sin(x = angleInDegrees * PI / 180f) * gap
+
+                val startY =
+                    (outerRadius * sin(angleInRadius) + circleCenter.y + yGapAdjustment).toFloat()
+
+                val start = Offset(
+                    x = (outerRadius * cos(angleInRadius) + circleCenter.x + xGapAdjustment).toFloat(),
+                    y = startY
+                )
+
+                val end = Offset(
+                    x = (outerRadius * cos(angleInRadius) + circleCenter.x + xGapAdjustment).toFloat(),
+                    y = (outerRadius * sin(angleInRadius) + circleThickness + circleCenter.y + yGapAdjustment).toFloat()
+                )
+
+                rotate(
+                    degrees = angleInDegrees,
+                    pivot = start
+                ) {
+                    drawLine(
+                        color = color,
+                        start = start,
+                        end = end,
+                        alpha = 0.9f,
+                        strokeWidth = 1.dp.toPx()
+                    )
+                }
+            }
+
+            drawContext.canvas.nativeCanvas.apply {
+                drawIntoCanvas {
+                    drawText(
+                        "$positionValue $valueUnit",
+                        circleCenter.x,
+                        circleCenter.y + 45.dp.toPx() / 3f,
+                        Paint().apply {
+                            textSize = 38.sp.toPx()
+                            textAlign = Paint.Align.CENTER
+                            color = Color.White.toArgb()
+                            isFakeBoldText = true
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CircularProgressIndicatorPreview() {
+    CustomCircularProgressIndicator(
+        modifier = Modifier
+            .size(250.dp)
+            .background(color = colorResource(id = R.color.darkGray)),
+        initialValue = 35,
+        valueUnit = "%",
+        primaryColor = colorResource(id = R.color.orange),
+        secondaryColor = colorResource(id = R.color.gray),
+        circularRadius = 230f,
+        onPositionChange = {
+
+        }
+    )
+}
