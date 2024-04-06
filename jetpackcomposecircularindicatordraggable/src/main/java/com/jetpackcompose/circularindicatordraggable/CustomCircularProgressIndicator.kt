5848e4983936f28c942f7aef338a7ac49241d562
Melchior Vrolijk <vrol0004@gmail.com>
2024-04-07 00:27:01 +0200

    ciruclar progress implemented and working just some bug to be fixed

--- jetpackcomposecircularindicatordraggable/src/main/java/com/jetpackcompose/circularindicatordraggable/CustomCircularProgressIndicator.kt
+++ jetpackcomposecircularindicatordraggable/src/main/java/com/jetpackcompose/circularindicatordraggable/CustomCircularProgressIndicator.kt
@@ -1,8 +1,10 @@
 package com.jetpackcompose.circularindicatordraggable
 
 import android.graphics.Paint
+import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -22,12 +24,15 @@ import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlin.math.PI
+import kotlin.math.atan2
 import kotlin.math.cos
+import kotlin.math.roundToInt
 import kotlin.math.sin
 
 @Composable
@@ -51,12 +56,59 @@ fun CustomCircularProgressIndicator(
         mutableStateOf(initialValue)
     }
 
+    var changeAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var dragStartedAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var oldPositionValue by remember {
+        mutableStateOf(initialValue)
+    }
+
     Box(
         modifier = modifier
     ) {
         Canvas(
             modifier = Modifier
                 .fillMaxSize()
+                .pointerInput(true) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            dragStartedAngle = -atan2(
+                                x = circleCenter.y - offset.y,
+                                y = circleCenter.x - offset.x
+                            ) * (180f / PI).toFloat()
+                            dragStartedAngle = (dragStartedAngle + 180f).mod(360f)
+                        },
+                        onDrag = { change, _ ->
+                            var touchAngle = -atan2(
+                                x = circleCenter.y - change.position.y,
+                                y = circleCenter.x - change.position.x
+                            ) * (180f / PI).toFloat()
+                            touchAngle = (touchAngle + 180f).mod(360f)
+
+                            val currentAngle = oldPositionValue * 360f / (maxValue-minValue)
+                            changeAngle = touchAngle - currentAngle
+
+                            val lowerThreshold = currentAngle - (360f / (maxValue-minValue) * 5)
+                            val higherThreshold = currentAngle + (360f / (maxValue-minValue) * 5)
+
+                            if (dragStartedAngle in lowerThreshold..higherThreshold) {
+                                positionValue = (oldPositionValue + (changeAngle/(360f/(maxValue-minValue))).roundToInt())
+                                Log.i("TAG12","position value: $positionValue")
+                                Log.i("TAG12","current angle: $currentAngle")
+                            }
+
+                        },
+                        onDragEnd = {
+                            oldPositionValue = positionValue
+                            onPositionChange(positionValue)
+                        }
+                    )
+                }
         ) {
 
             val width = size.width
@@ -168,7 +220,7 @@ fun CircularProgressIndicatorPreview() {
         modifier = Modifier
             .size(250.dp)
             .background(color = colorResource(id = R.color.darkGray)),
-        initialValue = 35,
+        initialValue = 30,
         valueUnit = "%",
         primaryColor = colorResource(id = R.color.orange),
         secondaryColor = colorResource(id = R.color.gray),
--- jetpackcomposecircularindicatordraggable/src/main/java/com/jetpackcompose/circularindicatordraggable/JetpackComposeDraggableCircularIndicatorMainActivity.kt
+++ jetpackcomposecircularindicatordraggable/src/main/java/com/jetpackcompose/circularindicatordraggable/JetpackComposeDraggableCircularIndicatorMainActivity.kt
@@ -3,13 +3,18 @@ package com.jetpackcompose.circularindicatordraggable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.jetpackcompose.circularindicatordraggable.ui.theme.DataSaverExampleAppTheme
 
 class JetpackComposeDraggableCircularIndicatorMainActivity : ComponentActivity() {
@@ -20,9 +25,26 @@ class JetpackComposeDraggableCircularIndicatorMainActivity : ComponentActivity()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = colorResource(id = R.color.darkGray)
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        CustomCircularProgressIndicator(
+                            modifier = Modifier
+                                .align(Alignment.Center)
+                                .size(250.dp)
+                                .background(color = colorResource(id = R.color.darkGray)),
+                            initialValue = 30,
+                            valueUnit = "%",
+                            primaryColor = colorResource(id = R.color.orange),
+                            secondaryColor = colorResource(id = R.color.gray),
+                            circularRadius = 230f,
+                            onPositionChange = { position ->
+
+                            }
+                        )
+                    }
                 }
             }
         }
